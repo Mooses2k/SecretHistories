65f5c30b3f0303c3301fd21a72e14dfedaef6083
crazy_stewie <joelgbsilva@gmail.com>
2022-05-12 23:34:11 -0300

    Initial door implementation (WIP)

---
+++ scenes/objects/world_objects/doors/door.gd
@@ -0,0 +1,68 @@
+extends Spatial
+class_name Door
+
+enum HingeSide {
+	LEFT,
+	RIGHT
+}
+
+export(HingeSide) var hinge_side : int = HingeSide.RIGHT setget set_hinge_side
+export var max_angle = 90.0 setget set_max_angle
+export var min_angle = 0.0 setget set_min_angle
+
+var nav_mesh : NavigationMeshInstance = null
+export var door_lock_count : int = 0 setget set_door_lock_count
+var door_locked_transform : Transform
+
+func set_door_lock_count(value : int):
+	if value == door_lock_count:
+		return
+	door_lock_count = value
+	if not is_inside_tree():
+		yield(self, "tree_entered")
+	if door_lock_count == 0: #unlock door
+		$DoorBody.mode = RigidBody.MODE_RIGID
+	else: #lock door
+		$DoorBody.transform = door_locked_transform
+		$DoorBody.mode = RigidBody.MODE_STATIC
+		
+func _enter_tree():
+	door_locked_transform = $DoorBody.transform
+
+func set_hinge_side(value : int):
+	if value == hinge_side:
+		return
+	hinge_side = value
+	if not is_inside_tree():
+		yield(self, "tree_entered")
+	var hinge_sign = -1 if hinge_side == HingeSide.RIGHT else 1.0
+	if sign($Hinge.translation.x) != hinge_sign:
+		$Hinge.translation.x *= -1
+	if sign($DoorBody/BottomHinge.translation.x) != hinge_sign:
+		$DoorBody/BottomHinge.translation.x *= -1
+	if sign($DoorBody/TopHinge.translation.x) != hinge_sign:
+		$DoorBody/TopHinge.translation.x *= -1
+	if sign($DoorBody/LockLoop.translation.x) != -hinge_sign:
+		$DoorBody/LockLoop.translation.x *= -1
+	if sign($DoorFrame/LockLoop.translation.x) != -hinge_sign:
+		$DoorFrame/LockLoop.translation.x *= -1
+	set_max_angle(max_angle)
+	set_min_angle(min_angle)
+		
+func set_max_angle(value : float):
+	max_angle = value
+	if not is_inside_tree():
+		yield(self, "tree_entered")
+	if hinge_side == HingeSide.RIGHT:
+		$DoorBody.max_angle = max_angle
+	else:
+		$DoorBody.min_angle = -max_angle
+
+func set_min_angle(value : float):
+	min_angle = value
+	if not is_inside_tree():
+		yield(self, "tree_entered")
+	if hinge_side == HingeSide.RIGHT:
+		$DoorBody.min_angle = min_angle
+	else:
+		$DoorBody.max_angle = -min_angle
---
+++ scenes/objects/world_objects/doors/door_body.gd
@@ -0,0 +1,32 @@
+extends RigidBody
+
+export var min_angle = 0.0
+export var max_angle = 90.0
+export var restitution = 0.5
+export var _hinge_node : NodePath
+onready var hinge_node : Spatial
+func _ready():
+	pass
+
+func _integrate_forces(state):
+#	state.angular_velocity = Vector3.ZERO
+#	return
+	if not hinge_node or not is_instance_valid(hinge_node):
+		hinge_node = get_node(_hinge_node)
+		if not hinge_node or not is_instance_valid(hinge_node):
+			return
+	var angle = wrapf(rotation.y, -PI, PI)
+	var target_angle = clamp(angle, deg2rad(min_angle), deg2rad(max_angle))
+	var angle_diff = wrapf(target_angle - angle, -PI, PI)
+	if not is_zero_approx(angle_diff):
+		var hinge_arm : Vector3 = state.transform.origin - hinge_node.global_transform.origin
+		hinge_arm.y = 0.0
+		var ang_vel : Vector3 = state.angular_velocity
+		print(ang_vel.y)
+		if sign(ang_vel.y) != sign(angle_diff):
+			ang_vel.y *= -restitution
+		state.angular_velocity = ang_vel
+		state.linear_velocity = ang_vel.cross(hinge_arm)
+		state.transform.basis = get_parent().global_transform.basis.rotated(Vector3.UP, target_angle)
+		
+		

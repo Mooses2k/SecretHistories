de7dbaa43d9493e8eb37c618e1923de5cb97af94
crazyStewie <joelgbsilva@gmail.com>
2022-06-30 22:13:22 -0300

    fix melee weapons damaging weapon wielder

--- scenes/objects/items/pickable/equipment/melee/cavalry_sabre/cavalry_sabre_item.gd
+++ scenes/objects/items/pickable/equipment/melee/cavalry_sabre/cavalry_sabre_item.gd
@@ -3,5 +3,5 @@ class_name SabreItem
 
 
 func _on_Hitbox_hit(other):
-	if can_hit and other.owner.has_method("damage"):
+	if can_hit and other.owner != owner_character and other.owner.has_method("damage"):
 		other.owner.damage(melee_damage, melee_damage_type, other)

99a4ceec7a085a7b5cdafec3c764dde3984e8efb
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2022-10-12 09:32:18 +0100

    Update cavalry

--- scenes/objects/items/pickable/equipment/melee/cavalry_sabre/cavalry_sabre_item.gd
+++ scenes/objects/items/pickable/equipment/melee/cavalry_sabre/cavalry_sabre_item.gd
@@ -8,9 +8,6 @@ func _on_Hitbox_hit(other):
 	if can_hit and other.owner != owner_character and other.owner.has_method("damage"):
 		other.owner.damage(melee_damage, melee_damage_type, other)
 
-
-
-
 func _on_Hitbox_body_entered(body):
 	if body is RigidBody and can_hit==true:
 		body.apply_central_impulse(-player.global_transform.basis.z*melee_damage)

02ea740ec9bce46a1d687395d5a2245a946d95e7
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2022-10-20 15:28:13 +0100

    Added equippables first round

---
+++ scenes/objects/items/pickable/equipment/components/rocks/Rock.gd
@@ -0,0 +1,14 @@
+extends EquipmentItem
+class_name Rock
+export (NodePath) var player_path
+export(AttackTypes.Types) var melee_damage_type : int = 0
+onready var player=get_node(player_path)
+func _ready():
+	pass
+func _on_Hitbox_hit(other):
+	pass
+
+
+
+func _on_Hitbox_body_entered(body):
+	pass

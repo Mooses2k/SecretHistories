84ac9044a62a087fa4259581c0d44d92654427cd
Chris <Moses2k@gmail.com>
2022-10-23 23:06:40 -1000

    Add crowbar, hatchet, halberd, dynamite, grenade

--- scenes/objects/items/pickable/equipment/melee/Dagger/Dagger.gd
+++ scenes/objects/items/pickable/equipment/melee/Dagger/Dagger.gd
@@ -1,13 +1,18 @@
-extends EquipmentItem
+extends MeleeItem
 class_name Dagger
+
+
 export (NodePath) var player_path
-export(AttackTypes.Types) var melee_damage_type : int = 0
-onready var player=get_node(player_path)
+
+onready var player = get_node(player_path)
+
+
 func _ready():
 	pass
+
+
 func _on_Hitbox_hit(other):
 	pass
-
 
 
 func _on_Hitbox_body_entered(body):

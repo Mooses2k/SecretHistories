75fc75558ab5ee96c91aa3dd053de5b87b6239e9
Mooses2k <moses2k@gmail.com>
2022-11-12 16:07:11 -0800

    torch and candle set for GLES3

--- scenes/objects/items/pickable/equipment/consumable/bomb/Dynamite/Dynamite.gd
+++ scenes/objects/items/pickable/equipment/consumable/bomb/Dynamite/Dynamite.gd
@@ -1,11 +1,14 @@
 extends ConsumableItem
 class_name Dynamite
 
+
 onready var countdown_timer = $Countdown
 var countdown_started = false
-# Called when the node enters the scene tree for the first time.
+
+
 func _ready():
 	print($Explosion.lifetime)
+
 
 func _process(delta):
 	if countdown_started == true:
@@ -13,6 +16,7 @@ func _process(delta):
 	if $Explosion.lifetime < 1.2:
 		queue_free()
 func _use_primary():
+
 
 	if countdown_timer.is_stopped():
 		countdown_timer.start()

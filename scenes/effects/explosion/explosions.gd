0bc4fc5301faf16fc93688331e1d656b2cc34b33
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2022-11-25 19:05:55 -0800

    Explosion update

--- scenes/effects/explosion/explosions.gd
+++ scenes/effects/explosion/explosions.gd
@@ -3,24 +3,34 @@ extends Particles
 
 onready var bombcasts = $"%bombcasts"
 export var shockwave : int 
+export var blast_range : float
+
 func _ready():
 	pass
 
 
 
 
-func _process(delta):
+
+func _physics_process(delta):
 	pass
 
-
 func _on_Bomb_explosion():
-	after_effects()
-
+	for ray in bombcasts.get_children():
+		ray.enabled = true
+		ray.cast_to.y = blast_range
+	$Shockwave.start()
 
 
 func after_effects():
-	for ray in bombcasts.get_children():
-		if ray.is_colliding():
-			var object = ray.get_collider()
-			if object is RigidBody:
-				object.apply_central_impulse(-ray.global_transform.basis.z*shockwave)
+		for ray in bombcasts.get_children():
+			if ray.is_colliding():
+				var object = ray.get_collider()
+				if object is RigidBody:
+					object.apply_central_impulse(-ray.global_transform.basis.z * shockwave)
+
+
+
+func _on_Shockwave_timeout():
+	after_effects()
+

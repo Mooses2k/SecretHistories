f454c4fbc063c84eeb199fc69d62d65c2801d6be
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2022-11-30 20:32:55 -0800

    Updated Explosion system

--- scenes/effects/explosion/explosions.gd
+++ scenes/effects/explosion/explosions.gd
@@ -1,10 +1,10 @@
 extends Particles
 
 
-onready var bombcasts = $"%bombcasts"
+onready var blastradius = $"%BlastRadius"
 export var shockwave : int 
-export var blast_range : float
 export var blast_damage : float
+var trigger = false
 export(AttackTypes.Types) var damage_type : int = 0
 
 func _ready():
@@ -18,25 +18,25 @@ func _physics_process(delta):
 	pass
 
 func _on_Bomb_explosion():
-	for ray in bombcasts.get_children():
-		ray.enabled = true
-		ray.cast_to.y = blast_range
-	$Shockwave.start()
-
-
-func after_effects():
-		for ray in bombcasts.get_children():
-			if ray.is_colliding():
-				var object = ray.get_collider() 
-				if object is RigidBody and !object.is_in_group("CHARACTER") :
-					object.apply_central_impulse(-ray.global_transform.basis.z * shockwave)
-				else:
-					object.apply_central_impulse(-ray.global_transform.basis.z * shockwave)
-					object.damage(blast_damage,damage_type,object)
+	var collisions = blastradius.get_overlapping_bodies()
+	for body in collisions:
+		if body is RigidBody:
+			var space = get_world().direct_space_state
+			var bodies = space.intersect_ray(global_transform.origin, body.global_transform.origin) 
+			if (not bodies.empty()) :
+				if bodies.collider is RigidBody:
+					if bodies.collider.is_in_group("CHARACTER"):
+						bodies.collider.damage(blast_damage,damage_type,bodies.collider)
+						bodies.collider.apply_central_impulse(-global_transform.basis.z * 2)
+					else:
+						bodies.collider.apply_central_impulse(bodies.position * shockwave)
 
 
 
 
-func _on_Shockwave_timeout():
-	after_effects()
 
+
+
+
+func _on_BlastRadius_body_entered(body):
+	pass

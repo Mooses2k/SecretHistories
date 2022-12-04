01651ee623d69af6578a59952fc61207807e4f27
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2022-12-03 21:37:30 -0800

    Better explosion effects

--- scenes/effects/explosion/explosions.gd
+++ scenes/effects/explosion/explosions.gd
@@ -19,22 +19,31 @@ func _on_Bomb_explosion():
 	for body in collisions:
 		if body is RigidBody:
 			var space = get_world().direct_space_state
-			var bodies = space.intersect_ray(global_transform.origin, body.global_transform.origin) 
+			var bodies = space.intersect_ray( body.global_transform.origin,global_transform.origin) 
+
 			if (not bodies.empty()) :
-				if ! detection_array.has(bodies.collider):
-					detection_array.append(bodies.collider)
-					var distance = global_transform.origin.distance_to(bodies.collider.global_transform.origin) 
-					var damage_coordinates 
-					if distance > 0 :
-						damage_coordinates = 1 / global_transform.origin.distance_to(bodies.collider.global_transform.origin) * get_parent().bomb_damage
+#				detection_array.append(bodies.collider)
+				var distance = global_transform.origin.distance_to(bodies.collider.global_transform.origin) 
+				var damage_coordinates 
+				if distance > 0 :
+					damage_coordinates = 1 / global_transform.origin.distance_to(bodies.collider.global_transform.origin) * get_parent().bomb_damage
+				else:
+					damage_coordinates = get_parent().bomb_damage
+				if bodies.collider != body and  bodies.collider  is RigidBody:
+					if body.is_in_group("CHARACTER"):
+						body.damage(damage_coordinates,get_parent().damage_type,bodies.collider)
+						body.apply_central_impulse( - body.global_transform.basis.z * damage_coordinates)
 					else:
-						damage_coordinates = get_parent().bomb_damage
-					if bodies.collider.is_in_group("CHARACTER") and bodies.collider is RigidBody:
-						bodies.collider.damage(damage_coordinates,get_parent().damage_type,bodies.collider)
-						bodies.collider.apply_central_impulse( -bodies.position * damage_coordinates)
-						print(damage_coordinates)
-					elif !bodies.collider.is_in_group("CHARACTER") and bodies.collider is RigidBody:
-						bodies.collider.apply_central_impulse( -bodies.position  * damage_coordinates)
+						body.apply_central_impulse( - body.global_transform.basis.z * damage_coordinates)
+						
+				else:
+#					if ! detection_array.has(bodies.collider):
+						if bodies.collider.is_in_group("CHARACTER") and bodies.collider is RigidBody:
+							bodies.collider.damage(damage_coordinates,get_parent().damage_type,bodies.collider)
+							bodies.collider.apply_central_impulse(- bodies.collider.global_transform.basis.z * damage_coordinates)
+							print(damage_coordinates)
+						elif !bodies.collider.is_in_group("CHARACTER") and bodies.collider is RigidBody:  
+							bodies.collider.apply_central_impulse(- bodies.collider.global_transform.basis.z * damage_coordinates)
 
 
 

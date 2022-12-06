faa456e4f80abdd9667e508368cc6b1cb0e65f16
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2022-12-05 19:18:24 -0800

    Explosion better

--- scenes/effects/explosion/explosions.gd
+++ scenes/effects/explosion/explosions.gd
@@ -30,18 +30,18 @@ func _on_Bomb_explosion():
 					damage_coordinates = get_parent().bomb_damage
 				if bodies.collider != body and  bodies.collider  is RigidBody:
 					if body.is_in_group("CHARACTER"):
-						body.damage(damage_coordinates,get_parent().damage_type,bodies.collider)
-						body.apply_central_impulse( + body.global_transform.basis.z * damage_coordinates)
+						body.damage(damage_coordinates * 2,get_parent().damage_type,bodies.collider)
+						body.apply_central_impulse( + body.global_transform.origin * 1 / global_transform.origin.distance_to(body.global_transform.origin) * get_parent().bomb_damage)
 					else:
-						body.apply_central_impulse( + body.global_transform.basis.z * damage_coordinates)
+						body.apply_central_impulse( + body.global_transform.origin * 1 / global_transform.origin.distance_to(body.global_transform.origin) * get_parent().bomb_damage)
 						
 				else:
 						if bodies.collider.is_in_group("CHARACTER") and bodies.collider is RigidBody:
 							bodies.collider.damage(damage_coordinates,get_parent().damage_type,bodies.collider)
-							bodies.collider.apply_central_impulse(+ bodies.collider.global_transform.basis.z * damage_coordinates)
+							bodies.collider.apply_central_impulse(+ bodies.collider.global_transform.origin * damage_coordinates)
 							
 						elif !bodies.collider.is_in_group("CHARACTER") and bodies.collider is RigidBody:  
-							bodies.collider.apply_central_impulse(+ bodies.collider.global_transform.basis.z * damage_coordinates)
+							bodies.collider.apply_central_impulse(+ bodies.collider.global_transform.origin * damage_coordinates)
 
 
 

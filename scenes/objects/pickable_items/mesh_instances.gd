f311cca83246b069ec7fa9cf0d2900c7237d6b88
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2022-12-06 20:34:22 -0800

    fixed weapon clipping

--- scenes/objects/pickable_items/mesh_instances.gd
+++ scenes/objects/pickable_items/mesh_instances.gd
@@ -1,14 +1,12 @@
 extends MeshInstance
 
-#
-#export (NodePath) var main_parent
-#onready var parent=get_node(main_parent)
-#func _ready():
-#	pass
-#
-#
-#func _process(delta):
-#	if parent.mode == parent.equipped_mode:
-#		layers=2
-#	else:
-#		layers=1
+
+onready var parent = get_owner()
+
+
+#Tracking the objects state , in order to prevent clipping
+func _process(delta):
+	if parent.mode == parent.equipped_mode:
+		layers = 2
+	else:
+		layers = 1

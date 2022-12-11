14d78cd94f3ea37d6e0612a7fa37ae4167b57e42
Chris <Moses2k@gmail.com>
2022-12-11 02:33:38 -0800

    fix candlelight on candelabras etc

---
+++ scenes/objects/pickable_items/equipment/tool/spike.gd
@@ -0,0 +1,6 @@
+extends ToolItem;
+class_name Spike
+
+
+# will eventually be stackable in 5, use be used with the tiny item, small_hammer,
+# to spike doors opened or closed; also ammo for a future gun

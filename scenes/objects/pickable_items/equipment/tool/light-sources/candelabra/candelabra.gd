359528c14d5503bab8c308124852f9757087110c
Chris <Moses2k@gmail.com>
2022-12-11 23:08:36 -0800

    fixing lighting and floor lighting, organizing, add medieval lantern

--- scenes/objects/pickable_items/equipment/tool/light-sources/candelabra/candelabra.gd
+++ scenes/objects/pickable_items/equipment/tool/light-sources/candelabra/candelabra.gd
@@ -1,56 +1,58 @@
-extends RigidBody
+extends ToolItem
 class_name CandelabraItem
+
 
 # eventually this is a tool/container-style item or large object that can be reloaded with candles which are disposable...not that you'd ever care to do that
 
-#
-#onready var firelight = $FireOrigin/Fire/Light
-#onready var DurableTimer = $Durability
-#
-#var has_ever_been_on = false 
-#var is_lit = false
-#
-#
+# this candelabra has NOT BEEN TESTED AT ALL
+
+onready var firelight = $Candle1/FireOrigin/Fire/Light
+onready var DurableTimer = $Durability
+
+var has_ever_been_on = false 
+var is_lit = false
+
+
 #func _ready():
 #	pass
-#
-#
-#func _process(delta):
-#	if is_lit == true:
-#		DurableTimer.pause_mode = false
-#	else:
-#		DurableTimer.pause_mode = true
-#	if has_ever_been_on == false:
-#			DurableTimer.start()
-#			has_ever_been_on = true
-#			firelight.visible = not firelight.visible
-#			$AnimationPlayer.play("flicker")
-#			$FireOrigin/Fire.emitting = true
-#			$FireOrigin/EmberDrip.emitting = true
-#			$FireOrigin/Smoke.emitting = true
-#			is_lit = true
-#	else:
-#		is_lit = false
-#
-#
-#func _use_primary():
-#	if !DurableTimer.is_stopped():
-#		firelight.visible = not firelight.visible
-#		$AnimationPlayer.play("flicker")
-#		$FireOrigin/Fire.emitting = not $FireOrigin/Fire.emitting
-#		$FireOrigin/EmberDrip.emitting = not $FireOrigin/EmberDrip.emitting
-#		$FireOrigin/Smoke.emitting = not $FireOrigin/Smoke
-#	else:
-#		firelight.visible = false
-#		$AnimationPlayer.stop()
-#		$FireOrigin/Fire.emitting = false
-#		$FireOrigin/EmberDrip.emitting = false
-#		$FireOrigin/Smoke.emitting = false
-#
-#
-#func _on_Durability_timeout():
-#	firelight.visible = false
-#	$AnimationPlayer.stop()
-#	$FireOrigin/Fire.emitting = false
-#	$FireOrigin/EmberDrip.emitting = false
-#	$FireOrigin/Smoke.emitting = false
+
+
+func _process(delta):
+	if is_lit == true:
+		DurableTimer.pause_mode = false
+	else:
+		DurableTimer.pause_mode = true
+	if has_ever_been_on == false:
+			DurableTimer.start()
+			has_ever_been_on = true
+			firelight.visible = not firelight.visible
+			$AnimationPlayer.play("flicker")
+			$Candle1/FireOrigin/Fire.emitting = true
+			$Candle2/FireOrigin/Fire.emitting = true
+			$Candle3/FireOrigin/Fire.emitting = true
+			is_lit = true
+	else:
+		is_lit = false
+
+
+func _use_primary():
+	if !DurableTimer.is_stopped():
+		firelight.visible = not firelight.visible
+		$AnimationPlayer.play("flicker")
+		$Candle1/FireOrigin/Fire.emitting = not $Candle1/FireOrigin/Fire.emitting
+		$Candle2/FireOrigin/Fire.emitting = not $Candle2/FireOrigin/Fire.emitting
+		$Candle3/FireOrigin/Fire.emitting = not $Candle3/FireOrigin/Fire.emitting
+	else:
+		firelight.visible = false
+		$AnimationPlayer.stop()
+		$Candle1/FireOrigin/Fire.emitting = false
+		$Candle2/FireOrigin/Fire.emitting = false
+		$Candle3/FireOrigin/Fire.emitting = false
+
+
+func _on_Durability_timeout():
+	firelight.visible = false
+	$AnimationPlayer.stop()
+	$Candle1/FireOrigin/Fire.emitting = false
+	$Candle2/FireOrigin/Fire.emitting = false
+	$Candle3/FireOrigin/Fire.emitting = false

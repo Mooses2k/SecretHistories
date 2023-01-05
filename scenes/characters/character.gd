2d4d35b81d4eb5f2ebc6965dc7c8f22a48258d82
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2023-01-05 11:15:17 -0800

    Updated changes and merged dev into branch

--- scenes/characters/character.gd
+++ scenes/characters/character.gd
@@ -462,6 +462,6 @@ func _on_ClamberableChecker_body_entered(body):
 
 func move_effect():
 	if move_dir:
-		additional_animations.play("Belt_bob", 0, move_speed / 6)
+		additional_animations.play("Belt_bob", -1, move_speed / 7)
 
 

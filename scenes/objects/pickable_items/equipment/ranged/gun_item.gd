89126a70f02b71643b86200c11fb3f823ee5e39f
Chris <Moses2k@gmail.com>
2023-02-19 22:25:10 -0800

    reload sounds (non-randomized) for all but complicated Lee  a46320

--- scenes/objects/pickable_items/equipment/ranged/gun_item.gd
+++ scenes/objects/pickable_items/equipment/ranged/gun_item.gd
@@ -96,6 +96,7 @@ func _use_reload():
 	reload()
 
 
+# needs more code for revolvers and bolt-actions as they're more complicated
 func reload():
 	if owner_character and current_ammo < ammunition_capacity and not is_reloading:
 		var inventory = owner_character.inventory
@@ -116,6 +117,10 @@ func reload():
 					_queued_reload_type = ammo_type
 					is_reloading = true
 					GameManager.is_reloading = true
+					
+					# eventually randomize which reload sound it uses
+					$Sounds/Reload.play()
+					
 					return
 
 

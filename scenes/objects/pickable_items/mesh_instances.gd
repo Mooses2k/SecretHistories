b50139b294ed335d39d827ae1a293266bac9f8fc
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2023-05-24 08:56:18 +0100

    updates to equipment items

--- scenes/objects/pickable_items/mesh_instances.gd
+++ scenes/objects/pickable_items/mesh_instances.gd
@@ -6,7 +6,11 @@ onready var parent = get_owner()
 
 # Tracking the objects state, in order to prevent clipping into walls when equipped
 func _process(delta):
-	if parent.mode == parent.equipped_mode:
-		layers = 2
+	if parent.name == "Player":
+		if parent.mode == parent.equipped_mode:
+			layers = 2
+		else:
+			layers = 1
+			
 	else:
 		layers = 1

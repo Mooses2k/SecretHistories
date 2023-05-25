aad846e95c0d096e6116cf7acbc7dac438cbce11
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2023-05-25 13:44:04 +0100

    more commits

--- scenes/objects/pickable_items/mesh_instances.gd
+++ scenes/objects/pickable_items/mesh_instances.gd
@@ -2,11 +2,15 @@ extends MeshInstance
 
 
 onready var parent = get_owner()
+onready var current_character = parent.get_parent().get_parent().get_parent()
 
+
+func _ready():
+	print(current_character.name)
 
 # Tracking the objects state, in order to prevent clipping into walls when equipped
 func _process(delta):
-	if parent.name == "Player":
+	if current_character.name == "Player":
 		if parent.mode == parent.equipped_mode:
 			layers = 2
 		else:

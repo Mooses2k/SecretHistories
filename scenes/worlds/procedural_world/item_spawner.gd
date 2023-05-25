6b3d71b4c439c22e434a87955c8081d66e190630
Daniel Queiroz Porto <danielvandalk@gmail.com>
2023-05-25 09:05:37 -0300

    clean up item spawner
    - encapsulated spawn tiny item into its own method
    - reordered methods a bit so that logic is easier to read, with the
    signal callback that is the "entry point" to this node at the top

--- scenes/worlds/procedural_world/item_spawner.gd
+++ scenes/worlds/procedural_world/item_spawner.gd
@@ -11,60 +11,50 @@ export var starting_light: PackedScene
 
 var free_cell = 0
 
+var _used_cell_indexes := []
 
-func get_next_free_cell(data : WorldData) -> bool:
+### Private Methods --------------------------------------------------------------------------------
+
+func _on_ProceduralWorld_generation_finished() -> void:
+	var data := owner.world_data as WorldData
+	_spawn_starting_light(data)
+	_spawn_initial_settings_items(data)
+
+
+func _spawn_initial_settings_items(data : WorldData):
+	var settings : SettingsClass = GameManager.game.local_settings
+	
+	_get_next_free_cell(data)
+	for s in settings.get_settings_list():
+		var g = settings.get_setting_group(s)
+		var amount = settings.get_setting(s)
+		
+		if g == "Equipment":
+			for i in amount:
+				if not _get_next_free_cell(data):
+					return
+				
+				_used_cell_indexes.append(free_cell)
+				var cell_pos = data.get_local_cell_position(free_cell)
+				_spawn_item(s, cell_pos)
+		elif g == "Tiny Items":
+			if amount == 0:
+				continue
+			if not _get_next_free_cell(data):
+				return
+			
+			_used_cell_indexes.append(free_cell)
+			var pos = data.get_local_cell_position(free_cell) + ITEM_POSITION_OFFSET
+			_spawn_tiny_item(s, amount, pos)
+
+
+func _get_next_free_cell(data : WorldData) -> bool:
 	free_cell += 1
 	while data.get_cell_type(free_cell) == data.CellType.EMPTY and free_cell < data.cell_count:
 		free_cell += 1
 	return true
 	if free_cell >= data.cell_count:
 		return false
-
-
-func spawn_items(data : WorldData):
-	var tiny_item_scene = preload("res://scenes/objects/pickable_items/tiny/_tiny_item.tscn")
-	var settings : SettingsClass = GameManager.game.local_settings
-	
-	_spawn_starting_light(data)
-	get_next_free_cell(data)
-	for s in settings.get_settings_list():
-		var g = settings.get_setting_group(s)
-		if g == "Equipment":
-			var amount = settings.get_setting(s)
-			for i in amount:
-				if not get_next_free_cell(data):
-					return
-				var cell_pos = data.get_local_cell_position(free_cell)
-				_spawn_item(s, cell_pos)
-		elif g == "Tiny Items":
-			var amount = settings.get_setting(s)
-			if amount == 0:
-				continue
-			if not get_next_free_cell(data):
-				return
-			var pos = data.get_local_cell_position(free_cell) + ITEM_POSITION_OFFSET
-			var item : TinyItem = tiny_item_scene.instance()
-			item.amount = amount
-			item.item_data = load(s)
-			item.translation = pos
-			owner.add_child(item)
-			pass
-
-
-func _spawn_starting_light(data : WorldData) -> void:
-	if starting_light == null:
-		return
-	
-	var starting_cells = data.get_cells_for(data.CellType.STARTING_ROOM)
-	if data.is_spawn_position_valid():
-		var player_index := data.get_player_spawn_position_as_index()
-		starting_cells.erase(player_index)
-	
-	var random_cell_index := randi() % starting_cells.size() as int
-	var spawn_cell = starting_cells[random_cell_index]
-	print("light spawned at: %s"%[data.get_int_position_from_cell_index(spawn_cell)])
-	var local_pos = data.get_local_cell_position(spawn_cell)
-	_spawn_item(starting_light.resource_path, local_pos)
 
 
 func _spawn_item(scene_path: String, cell_pos: Vector3) -> void:
@@ -78,6 +68,30 @@ func _spawn_item(scene_path: String, cell_pos: Vector3) -> void:
 	owner.add_child(item)
 
 
-func _on_ProceduralWorld_generation_finished() -> void:
-	spawn_items(owner.world_data)
-	pass # Replace with function body.
+func _spawn_tiny_item(item_data_path: String, amount: int, position: Vector3) -> void:
+	var tiny_item_scene = preload("res://scenes/objects/pickable_items/tiny/_tiny_item.tscn")
+	var item : TinyItem = tiny_item_scene.instance()
+	item.amount = amount
+	item.item_data = load(item_data_path)
+	item.translation = position
+	owner.add_child(item)
+
+
+func _spawn_starting_light(data : WorldData) -> void:
+	if starting_light == null:
+		return
+	
+	var starting_cells = data.get_cells_for(data.CellType.STARTING_ROOM)
+	if data.is_spawn_position_valid():
+		var player_index := data.get_player_spawn_position_as_index()
+		starting_cells.erase(player_index)
+	
+	var random_cell_index := randi() % starting_cells.size() as int
+	var spawn_cell = starting_cells[random_cell_index]
+	_used_cell_indexes.append(spawn_cell)
+	print("light spawned at: %s"%[data.get_int_position_from_cell_index(spawn_cell)])
+	
+	var local_pos = data.get_local_cell_position(spawn_cell)
+	_spawn_item(starting_light.resource_path, local_pos)
+
+### ------------------------------------------------------------------------------------------------

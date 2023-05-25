b5b66b3106f8688d3e90475a94825823e628e03f
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2023-05-25 20:17:56 +0100

    Shotgun now showing

--- scenes/objects/pickable_items/mesh_instances.gd
+++ scenes/objects/pickable_items/mesh_instances.gd
@@ -2,19 +2,20 @@ extends MeshInstance
 
 
 onready var parent = get_owner()
-onready var current_character = parent.get_parent().get_parent().get_parent()
 
 
 func _ready():
-	print(current_character.name)
-
+	pass
 # Tracking the objects state, in order to prevent clipping into walls when equipped
 func _process(delta):
-	if current_character.name == "Player":
-		if parent.mode == parent.equipped_mode:
-			layers = 2
+	if parent.get_parent().owner != null:
+#		print(parent.get_parent().owner.name)
+		if parent.get_parent().owner.name == "Player":
+		#	if parent.mode == parent.equipped_mode:
+				layers = 2
 		else:
-			layers = 1
+				layers = 1
 			
 	else:
 		layers = 1
+

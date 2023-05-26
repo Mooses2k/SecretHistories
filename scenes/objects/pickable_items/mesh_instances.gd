e3153b66a1aa36816306949412b8e8dfba6e23be
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2023-05-26 03:20:31 +0100

    3 fixes (#425)
    * Fps camera removed from character scene
    
    * some few cultist changes
    
    * Cultist rifle animations fixed
    
    * updates to equipment items
    
    * more commits
    
    * Shotgun now showing
    
    * updates
    
    * Update procedural_world.tscn

--- scenes/objects/pickable_items/mesh_instances.gd
+++ scenes/objects/pickable_items/mesh_instances.gd
@@ -4,9 +4,18 @@ extends MeshInstance
 onready var parent = get_owner()
 
 
+func _ready():
+	pass
 # Tracking the objects state, in order to prevent clipping into walls when equipped
 func _process(delta):
-	if parent.mode == parent.equipped_mode:
-		layers = 2
+	if parent.get_parent().owner != null:
+#		print(parent.get_parent().owner.name)
+		if parent.get_parent().owner.name == "Player":
+		#	if parent.mode == parent.equipped_mode:
+				layers = 2
+		else:
+				layers = 1
+			
 	else:
 		layers = 1
+

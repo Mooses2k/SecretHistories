8871b7bd92ed331e34753252419218c9ce6dc711
Daniel Queiroz Porto <danielvandalk@gmail.com>
2023-05-30 20:07:13 -0300

    Add GenerationGroup class for nesting GenerationSteps

---
+++ scenes/worlds/world_gen/generation_steps/generation_group.gd
@@ -0,0 +1,49 @@
+# Acts as a Group of GenerationSteps, it will execute all of it's children before moving on
+class_name GenerationGroup
+extends GenerationStep
+
+### Member Variables and Dependencies -------------------------------------------------------------
+#--- signals --------------------------------------------------------------------------------------
+
+#--- enums ----------------------------------------------------------------------------------------
+
+#--- constants ------------------------------------------------------------------------------------
+
+#--- public variables - order: export > normal var > onready --------------------------------------
+
+#--- private variables - order: export > normal var > onready -------------------------------------
+
+### -----------------------------------------------------------------------------------------------
+
+
+### Built-in Virtual Overrides --------------------------------------------------------------------
+
+### -----------------------------------------------------------------------------------------------
+
+
+### Public Methods --------------------------------------------------------------------------------
+
+func execute_step(data : WorldData, gen_data : Dictionary, generation_seed : int):
+	_execute_step(data, gen_data, generation_seed)
+	pass
+
+### -----------------------------------------------------------------------------------------------
+
+
+### Private Methods -------------------------------------------------------------------------------
+
+func _execute_step(data : WorldData, gen_data : Dictionary, generation_seed : int):
+	for child in get_children():
+		if child is GenerationStep:
+			child.execute_step(data, gen_data, generation_seed)
+		else:
+			push_error("Node (%s) is not GenerationStep and is inside GenerationGroup. Path:%s"%[
+					child, (child as Node).get_path()
+			])
+
+### -----------------------------------------------------------------------------------------------
+
+
+### Signal Callbacks ------------------------------------------------------------------------------
+
+### -----------------------------------------------------------------------------------------------

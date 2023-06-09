c3b41626712811b0979edb525a930b7c43e9e4db
Daniel Queiroz Porto <danielvandalk@gmail.com>
2023-06-09 07:33:17 -0300

    Add GenerateInitialLoot step and refactor spawn data...
    ... so that you can set transforms on spawn data per amount

--- scenes/worlds/procedural_world/item_spawner.gd
+++ scenes/worlds/procedural_world/item_spawner.gd
@@ -46,8 +46,7 @@ func _spawn_world_data_objects(data: WorldData) -> void:
 	var objects_to_spawn := data.get_objects_to_spawn()
 	for cell_index in objects_to_spawn:
 		var spawn_data := objects_to_spawn[cell_index] as SpawnData
-		for _i in spawn_data.amount:
-			spawn_data.spawn_item_in(owner)
+		spawn_data.spawn_item_in(owner, true)
 
 
 # This calculates the center position of the cell and then tries to find a random position 
--- scenes/worlds/procedural_world/spawn_data.gd
+++ scenes/worlds/procedural_world/spawn_data.gd
@@ -1,4 +1,5 @@
 # Helper class for spawning objects
+tool
 class_name SpawnData
 extends Resource
 
@@ -14,10 +15,11 @@ const CENTER_POSITION_OFFSET = Vector3(0.75, 1.0, 0.75)
 #--- public variables - order: export > normal var > onready --------------------------------------
 
 export var scene_path: String = ""
-export var amount: int = 1
-export var transform := Transform.IDENTITY
+export var amount: int = 1 setget _set_amount
 
 #--- private variables - order: export > normal var > onready -------------------------------------
+
+export var _transforms: Array
 
 ### -----------------------------------------------------------------------------------------------
 
@@ -34,21 +36,29 @@ func _to_string() -> String:
 ### Public Methods --------------------------------------------------------------------------------
 
 func spawn_item_in(node: Node, should_log := false) -> void:
-	var item
 	var item_scene : PackedScene = load(scene_path)
-	item = item_scene.instance()
-	if item is Spatial:
-		item.transform = transform
+	for index in amount:
+		var item = item_scene.instance()
+		if item is Spatial:
+			item.transform = _transforms[index]
+		
+		node.add_child(item, true)
+		if should_log:
+			print("item spawned: %s | at: %s | rotated by: %s"%[
+					scene_path, _transforms[index].origin, _transforms[index].basis.get_euler()
+			])
+
+
+func set_center_position_in_cell(cell_position: Vector3, instance_index := INF) -> void:
+	if amount > 1 and instance_index == INF:
+		push_warning("Setting the smae position for multiple item instances")
 	
-	node.add_child(item, true)
-	if should_log:
-		print("item spawned: %s | at: %s | rotated by: %s"%[
-				scene_path, transform.origin, transform.basis.get_euler()
-		])
-
-
-func set_center_position_in_cell(cell_position: Vector3) -> void:
-	transform = transform.translated(cell_position + CENTER_POSITION_OFFSET)
+	for i in amount:
+		if instance_index != INF and i != instance_index:
+			continue
+		
+		var transform := Transform.IDENTITY.translated(cell_position + CENTER_POSITION_OFFSET)
+		_transforms[i] = transform
 
 
 # This calculates the center position of the cell and then tries to find a random position 
@@ -58,36 +68,71 @@ func set_random_position_in_cell(
 		cell_position: Vector3, 
 		min_radius: float, 
 		max_radius: float, 
-		angle := INF
+		p_angle := INF,
+		instance_index := INF
 ) -> void:
-	var center_position := cell_position + CENTER_POSITION_OFFSET
-	if angle == INF:
-		angle = rng.randf_range(0.0, TAU)
+	if p_angle != INF and instance_index == INF:
+		push_warning("Setting the same position for multiple item instances")
 	
-	var radius := rng.randf_range(min_radius, max_radius)
-	var random_direction := Vector3(cos(angle), 0.0, sin(angle)).normalized()
-	var polar_coordinate := random_direction * radius
-	
-	var random_position := center_position + polar_coordinate
-	transform = transform.translated(random_position)
+	for i in amount:
+		if instance_index != INF and i != instance_index:
+			continue
+		
+		var transform := _transforms[i] as Transform
+		var angle := p_angle
+		var center_position := cell_position + CENTER_POSITION_OFFSET
+		
+		if angle == INF:
+			angle = rng.randf_range(0.0, TAU)
+			print("angle #%s: %s"%[i, angle])
+		
+		var radius := rng.randf_range(min_radius, max_radius)
+		var random_direction := Vector3(cos(angle), 0.0, sin(angle)).normalized()
+		var polar_coordinate := random_direction * radius
+		var random_position := center_position + polar_coordinate
+		transform = transform.translated(random_position)
+		transform.basis = transform.basis.rotated(Vector3.UP, angle)
+		_transforms[i] = transform
 
 
-func set_random_rotation_in_all_axis(rng: RandomNumberGenerator) -> void:
-	var random_rotation = Quat(Vector3(
-		rng.randf_range(0, TAU),
-		rng.randf_range(0, TAU),
-		rng.randf_range(0, TAU)
-	))
-	transform.basis = Basis(random_rotation)
+func set_random_rotation_in_all_axis(rng: RandomNumberGenerator, instance_index := INF) -> void:
+	for i in amount:
+		if instance_index != INF and i != instance_index:
+			continue
+		
+		var transform = _transforms[i] as Transform
+		var random_rotation = Quat(Vector3(
+			rng.randf_range(0, TAU),
+			rng.randf_range(0, TAU),
+			rng.randf_range(0, TAU)
+		))
+		transform.basis = Basis(random_rotation)
+		_transforms[i] = transform
 
 
-func set_y_rotation(angle_rad: float) -> void:
-	transform.basis = transform.basis.rotated(Vector3.UP, angle_rad)
+func set_y_rotation(angle_rad: float, instance_index := INF) -> void:
+	for i in amount:
+		if instance_index != INF and i != instance_index:
+			continue
+		
+		var transform = _transforms[i] as Transform
+		transform.basis = transform.basis.rotated(Vector3.UP, angle_rad)
+		_transforms[i] = transform
 
 ### -----------------------------------------------------------------------------------------------
 
 
 ### Private Methods -------------------------------------------------------------------------------
+
+func _set_amount(value: int) -> void:
+	amount = max(1, value)
+	var old_tranforms = _transforms.duplicate()
+	_transforms.resize(amount)
+	_transforms.fill(Transform.IDENTITY)
+	
+	for index in _transforms.size():
+		if index < old_tranforms.size() and _transforms[index] != old_tranforms[index]:
+			_transforms[index] = old_tranforms[index]
 
 ### -----------------------------------------------------------------------------------------------
 

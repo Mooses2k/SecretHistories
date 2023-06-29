26a53f032e2770bb4566259f636c14ef19bea885
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2023-06-29 23:47:34 +0100

    More improvements

--- scenes/characters/player/player_anims.gd
+++ scenes/characters/player/player_anims.gd
@@ -130,12 +130,12 @@ func _physics_process(delta):
 func check_player_animation():
 	print(inventory.current_offhand_equipment)
 	adjust_arm()
-	if inventory.current_mainhand_equipment != null:
+	if inventory.current_mainhand_equipment != null and inventory.current_offhand_equipment == null :
+		animation_tree.set("parameters/OffHand_MainHand_Blend/blend_amount",0)
+	if inventory.current_offhand_equipment != null and inventory.current_mainhand_equipment == null :
 		animation_tree.set("parameters/OffHand_MainHand_Blend/blend_amount",1)
-	if inventory.current_offhand_equipment != null:
+	elif inventory.current_offhand_equipment != null and inventory.current_mainhand_equipment != null:
 		animation_tree.set("parameters/OffHand_MainHand_Blend/blend_amount",1)
-	else:
-		offhand_active = false
 		
 #	check_equipped_hands()
 	# This code checks the current item type the player is equipping and set the animation that matches that item in the animation tree
@@ -144,21 +144,21 @@ func check_player_animation():
 	if inventory.current_offhand_equipment is GunItem:
 		
 		animation_tree.set("parameters/Hand_Transition/current",0)
-		animation_tree.set("parameters/OffHand_MainHand_Blend/blend_amount",1)
-		animation_tree.set("parameters/OffHand_MainHand_Blend/blend_amount",1)
+#		animation_tree.set("parameters/OffHand_MainHand_Blend/blend_amount",1)
+#		animation_tree.set("parameters/OffHand_MainHand_Blend/blend_amount",1)
 		animation_tree.set("parameters/OffHand_Weapon_States/current",1)
 		
 	elif inventory.current_offhand_equipment is EquipmentItem:
 		if inventory.current_offhand_equipment.horizontal_holding == true:
 			inventory.current_offhand_equipment.hold_position.rotation_degrees.z = -90
 			animation_tree.set("parameters/Hand_Transition/current",0)
-			animation_tree.set("parameters/OffHand_MainHand_Blend/blend_amount",1)
+#			animation_tree.set("parameters/OffHand_MainHand_Blend/blend_amount",1)
 			animation_tree.set("parameters/OffHand_Weapon_States/current",0)
 			animation_tree.set("parameters/Offhand_Hold_Animation/current",1)
 			
 		else:
 			animation_tree.set("parameters/Hand_Transition/current",0)
-			animation_tree.set("parameters/OffHand_MainHand_Blend/blend_amount",1)
+#			animation_tree.set("parameters/OffHand_MainHand_Blend/blend_amount",1)
 			animation_tree.set("parameters/OffHand_Weapon_States/current",0)
 			animation_tree.set("parameters/Offhand_Hold_Animation/current",0)
 
@@ -177,7 +177,7 @@ func check_player_animation():
 #
 	elif inventory.current_offhand_equipment == EmptyHand:
 		animation_tree.set("parameters/Hand_Transition/current",0)
-		animation_tree.set("parameters/OffHand_MainHand_Blend/blend_amount",1)
+#		animation_tree.set("parameters/OffHand_MainHand_Blend/blend_amount",1)
 		animation_tree.set("parameters/OffHand_Weapon_States/current",2)
 
 

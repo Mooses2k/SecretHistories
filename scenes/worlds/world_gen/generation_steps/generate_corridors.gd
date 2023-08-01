bc95dbd485839cf48beb329bba7301802cf60f25
Daniel Queiroz Porto <danielvandalk@gmail.com>
2023-07-31 21:30:33 -0300

    clean up generate_corridor script
    Adds more named constants, change variable names, and split functions
    so that the code auto documents itself a little better

--- scenes/worlds/world_gen/generation_steps/generate_corridors.gd
+++ scenes/worlds/world_gen/generation_steps/generate_corridors.gd
@@ -1,14 +1,52 @@
 extends GenerationStep
 
+### Member Variables and Dependencies -------------------------------------------------------------
+#--- signals --------------------------------------------------------------------------------------
+
+#--- enums ----------------------------------------------------------------------------------------
+
+enum AstarType {
+	EMPTY,
+	ROOM,
+	EDGE,
+}
+
+enum CorridorGenerationResults {
+	FAILED,
+	DOOR,
+	GOAL,
+}
+
+#--- constants ------------------------------------------------------------------------------------
+
 const GraphGenerator = preload("generate_room_graph.gd")
+
+const MASK_EMPTY = 0b0000
+const MASK_ROOM_TOP_LEFT = 0b0001
+const MASK_ROOM_TOP_RIGHT = 0b0010
+const MASK_ROOM_BOTTOM_RIGHT = 0b0100
+const MASK_ROOM_BOTTOM_LEFT = 0b1000
+
+const MASK_ROOM_NORTH = MASK_ROOM_TOP_LEFT | MASK_ROOM_TOP_RIGHT
+const MASK_ROOM_EAST = MASK_ROOM_TOP_RIGHT | MASK_ROOM_BOTTOM_RIGHT
+const MASK_ROOM_SOUTH = MASK_ROOM_BOTTOM_LEFT | MASK_ROOM_BOTTOM_RIGHT
+const MASK_ROOM_WEST = MASK_ROOM_TOP_LEFT | MASK_ROOM_BOTTOM_LEFT
+const MASK_FULL_ROOM = 0b1111
+
+#--- public variables - order: export > normal var > onready --------------------------------------
 
 export var existing_corridor_weight : float = 0.5
 export var existing_room_weight : float = 2.0
 export var room_edge_cost_multiplier : float = 1.5
 
+#--- private variables - order: export > normal var > onready -------------------------------------
 
-# Override this function
-func _execute_step(data : WorldData, gen_data : Dictionary, generation_seed : int):
+### -----------------------------------------------------------------------------------------------
+
+
+### GenerationStep Virtual Overrides --------------------------------------------------------------------
+
+func _execute_step(data : WorldData, gen_data : Dictionary, _generation_seed : int):
 	var graph = gen_data.get(GraphGenerator.CONNECTION_GRAPH_KEY, Dictionary()) as Dictionary
 	var astar = generate_double_a_star_grid(data)
 	for a in graph.keys():
@@ -16,102 +54,28 @@ func _execute_step(data : WorldData, gen_data : Dictionary, generation_seed : in
 			generate_double_corridor(data, astar, a, b)
 	pass
 
+### -----------------------------------------------------------------------------------------------
 
-func get_cell_mask(data : WorldData, cells : Array, value: int) -> int:
-	var mask = 0b0000
-	mask = mask | 0b0001*int(data.get_cell_type(cells[0]) == value)
-	mask = mask | 0b0010*int(data.get_cell_type(cells[1]) == value)
-	mask = mask | 0b0100*int(data.get_cell_type(cells[2]) == value)
-	mask = mask | 0b1000*int(data.get_cell_type(cells[3]) == value)
-	return mask
 
+### Public Methods --------------------------------------------------------------------------------
 
 func generate_double_a_star_grid(data : WorldData) -> AStar2D:
 	var astar = ManhattanAStar2D.new()
-	for x in range(2, data.world_size_x - 1):
-		for z in range(2, data.world_size_z - 1):
-			var cells = [
-				data.get_cell_index_from_int_position(x - 1, z - 1),
-				data.get_cell_index_from_int_position(x, z - 1),
-				data.get_cell_index_from_int_position(x, z),
-				data.get_cell_index_from_int_position(x - 1, z),
-			]
-			var room_mask = get_cell_mask(data, cells, data.CellType.ROOM)
-			var p : Vector2 = Vector2(x, z)
-			
-			var w = 1.0
-			# fully outside of a room
-			if room_mask == 0b0000:
-				w = 1.0
-			# fully inside of a room
-			elif room_mask == 0b1111:
-				w = existing_room_weight
-			# at the edge of a room
-			elif room_mask == 0b0011 or room_mask == 0b0110 or room_mask == 0b1100 or room_mask == 0b1001:
-				# high weight to make crossing the edge mostly perpendicular
-				w = room_edge_cost_multiplier*existing_room_weight
-			# at the corner of one or more rooms (internal or external)
-			else:
-				# don't add corner points
-				continue
-			astar.add_point(cells[2], p, w)
-	for x in range(1, data.world_size_x-1):
-		for z in range(1, data.world_size_z-1):
-			var i = data.get_cell_index_from_int_position(x, z)
-			if not astar.has_point(i):
-				continue
-			if x != data.world_size_x - 1:
-				var e = data.get_neighbour_cell(i, data.Direction.EAST)
-				if astar.has_point(e):
-					astar.connect_points(i, e)
-			if x != 1:
-				var w = data.get_neighbour_cell(i, data.Direction.WEST)
-				if astar.has_point(w):
-					astar.connect_points(i, w)
-			if z != data.world_size_z - 1:
-				var s = data.get_neighbour_cell(i, data.Direction.SOUTH)
-				if astar.has_point(s):
-					astar.connect_points(i, s)
-			if z != 1:
-				var n = data.get_neighbour_cell(i, data.Direction.NORTH)
-				if astar.has_point(n):
-					astar.connect_points(i, n)
+	_add_weighted_points_to_astar_grid(data, astar)
+	_connect_points_in_astar_grid(data, astar)
 	return astar
-
-
-func set_cells(data : WorldData, cells : Array, values : Array):
-	for i in cells.size():
-		if values [i] > data.get_cell_type(cells[i]):
-			data.set_cell_type(cells[i], values[i])
-
-
-func add_door_direction(data : WorldData, cell : int, value : int):
-	if not data.has_cell_meta(cell, data.CellMetaKeys.META_DOOR_DIRECTIONS):
-		data.set_cell_meta(cell, data.CellMetaKeys.META_DOOR_DIRECTIONS, Array())
-	if not data.get_cell_meta(cell, data.CellMetaKeys.META_DOOR_DIRECTIONS).has(value):
-		data.get_cell_meta(cell, data.CellMetaKeys.META_DOOR_DIRECTIONS).push_back(value)
-
-
-func set_doorways_meta(data: WorldData, cells: Array, direction: int) -> void:
-	if cells.empty():
-		return
-	
-	for cell in cells:
-		if data.get_cell_type(cell) == data.CellType.ROOM:
-			var room_data := data.get_cell_meta(cell, data.CellMetaKeys.META_ROOM_DATA) as RoomData
-			room_data.set_doorway_cell(cell, data.direction_inverse(direction))
-		elif data.get_cell_type(cell) == data.CellType.DOOR:
-			add_door_direction(data, cell, direction)
 
 
 func generate_double_corridor(data : WorldData, astar : AStar2D, a : int, b : int) -> bool:
 	var path : PoolIntArray = astar.get_id_path(a, b)
 	if not (path.size() > 0 and path[0] == a and path[-1] == b):
-		print(a)
-		print(data.get_int_position_from_cell_index(a))
-		print(b)
-		print(data.get_int_position_from_cell_index(b))
+		print("a: %s (%s) | b: %s (%s) | path[0]: %s | path[-1]: %s"%[
+				a, data.get_int_position_from_cell_index(a), 
+				b, data.get_int_position_from_cell_index(b),
+				path[0], path[-1]
+		])
 		return false
+	
 	for i in path.size():
 		var coords = data.get_int_position_from_cell_index(path[i])
 		var x = coords[0]
@@ -121,49 +85,51 @@ func generate_double_corridor(data : WorldData, astar : AStar2D, a : int, b : in
 				data.get_cell_index_from_int_position(x, z - 1),
 				data.get_cell_index_from_int_position(x, z),
 				data.get_cell_index_from_int_position(x - 1, z),
-			]
+		]
 		# room mask as 3210 (clockwise order from lower bit)
-		var rooms = get_cell_mask(data, cells, data.CellType.ROOM)
+		var mask_2x2 = _get_cell_mask(data, cells, data.CellType.ROOM)
 		
-		var is_edge = false   # Yellow warning in editor; var never used
-		var is_room = false
-		match rooms:
-			0b0000:
-				set_cells(data, cells, [
+		var astar_type := AstarType.EMPTY as int
+		match mask_2x2:
+			MASK_EMPTY:
+				_set_cells(data, cells, [
 						data.CellType.CORRIDOR, data.CellType.CORRIDOR, 
 						data.CellType.CORRIDOR, data.CellType.CORRIDOR
 				])
-			0b0011:
-				set_cells(data, cells, [
+			MASK_ROOM_NORTH:
+				_set_cells(data, cells, [
 						data.CellType.ROOM, data.CellType.ROOM, 
 						data.CellType.DOOR, data.CellType.DOOR
 				])
-				set_doorways_meta(data, cells, data.Direction.NORTH)
-				is_edge = true
-			0b0110:
-				set_cells(data, cells, [
+				_set_doorways_meta(data, cells, data.Direction.NORTH)
+				astar_type = AstarType.EDGE
+			MASK_ROOM_EAST:
+				_set_cells(data, cells, [
 						data.CellType.DOOR, data.CellType.ROOM, 
 						data.CellType.ROOM, data.CellType.DOOR
 				])
-				set_doorways_meta(data, cells, data.Direction.EAST)
-				is_edge = true
-			0b1100:
-				set_cells(data, cells, [
+				_set_doorways_meta(data, cells, data.Direction.EAST)
+				astar_type = AstarType.EDGE
+			MASK_ROOM_SOUTH:
+				_set_cells(data, cells, [
 						data.CellType.DOOR, data.CellType.DOOR, 
 						data.CellType.ROOM, data.CellType.ROOM
 				])
-				set_doorways_meta(data, cells, data.Direction.SOUTH)
-				is_edge = true
-			0b1001:
-				set_cells(data, cells, [
+				_set_doorways_meta(data, cells, data.Direction.SOUTH)
+				astar_type = AstarType.EDGE
+			MASK_ROOM_WEST:
+				_set_cells(data, cells, [
 						data.CellType.ROOM, data.CellType.DOOR, 
 						data.CellType.DOOR, data.CellType.ROOM]
 				)
-				set_doorways_meta(data, cells, data.Direction.WEST)
-				is_edge = true
-			0b1111:
-				is_room = true
-		if not is_room:
+				_set_doorways_meta(data, cells, data.Direction.WEST)
+				astar_type = AstarType.EDGE
+			MASK_FULL_ROOM:
+				astar_type = AstarType.ROOM
+		
+		if astar_type == AstarType.EDGE:
+			astar.set_point_weight_scale(cells[2], existing_corridor_weight * room_edge_cost_multiplier)
+		elif astar_type == AstarType.EMPTY:
 			astar.set_point_weight_scale(cells[2], existing_corridor_weight)
 	
 	var rooms := [
@@ -174,15 +140,120 @@ func generate_double_corridor(data : WorldData, astar : AStar2D, a : int, b : in
 	for room_value in rooms:
 		var room := room_value as RoomData
 		if not room.can_add_doorway():
-			var doorway_cells := room.get_all_doorway_cells()
-			for cell_index in room.cell_indexes:
-				for direction in data.Direction.values():
-					if direction == data.Direction.DIRECTION_MAX or cell_index in doorway_cells:
-						continue
-					
-					var neighbour_index := data.get_neighbour_cell(cell_index, direction)
-					if not neighbour_index in room.cell_indexes:
-						if astar.are_points_connected(cell_index, neighbour_index):
-							astar.disconnect_points(cell_index, neighbour_index)
+			_disconnect_room_walls_from_grid(data, astar, room)
 	
 	return true
+
+### -----------------------------------------------------------------------------------------------
+
+
+### Private Methods -------------------------------------------------------------------------------
+
+func _add_weighted_points_to_astar_grid(data: WorldData, astar: AStar2D) -> void:
+	for x in range(1, data.world_size_x - 1):
+		for z in range(1, data.world_size_z - 1):
+			var cells = [
+				data.get_cell_index_from_int_position(x - 1, z - 1),
+				data.get_cell_index_from_int_position(x, z - 1),
+				data.get_cell_index_from_int_position(x, z),
+				data.get_cell_index_from_int_position(x - 1, z),
+			]
+			
+			var room_mask = _get_cell_mask(data, cells, data.CellType.ROOM)
+			var point := Vector2(x, z)
+			var weight := _get_weight_for(room_mask)
+			if weight > 0:
+				astar.add_point(cells[2], point, weight)
+
+
+func _get_cell_mask(data : WorldData, cells : Array, value: int) -> int:
+	var mask = MASK_EMPTY
+	mask = mask | MASK_ROOM_TOP_LEFT * int(data.get_cell_type(cells[0]) == value)
+	mask = mask | MASK_ROOM_TOP_RIGHT * int(data.get_cell_type(cells[1]) == value)
+	mask = mask | MASK_ROOM_BOTTOM_RIGHT * int(data.get_cell_type(cells[2]) == value)
+	mask = mask | MASK_ROOM_BOTTOM_LEFT * int(data.get_cell_type(cells[3]) == value)
+	return mask
+
+
+func _get_weight_for(room_mask: int) -> float:
+	var weight = 1.0
+	
+	# fully outside of a room
+	if room_mask == MASK_EMPTY:
+		weight = 1.0
+	# fully inside of a room
+	elif room_mask == MASK_FULL_ROOM:
+		weight = existing_room_weight
+	# at the edge of a room
+	elif (
+			room_mask == MASK_ROOM_NORTH 
+			or room_mask == MASK_ROOM_EAST 
+			or room_mask == MASK_ROOM_SOUTH 
+			or room_mask == MASK_ROOM_WEST
+	):
+		# high weight to make crossing the edge mostly perpendicular
+		weight = room_edge_cost_multiplier * existing_room_weight
+	# at the corner of one or more rooms (internal or external)
+	else:
+		weight = 0
+	
+	return weight
+
+
+func _connect_points_in_astar_grid(data: WorldData, astar: AStar2D) -> void:
+	for x in range(1, data.world_size_x-1):
+		for z in range(1, data.world_size_z-1):
+			var cell_index = data.get_cell_index_from_int_position(x, z)
+			if not astar.has_point(cell_index):
+				continue
+			
+			for direction in data.Direction.DIRECTION_MAX:
+				var neighbour = data.get_neighbour_cell(cell_index, direction)
+				if neighbour != -1 and astar.has_point(neighbour):
+					astar.connect_points(cell_index, neighbour)
+
+
+func _set_cells(data : WorldData, cells : Array, values : Array):
+	for i in cells.size():
+		if values [i] > data.get_cell_type(cells[i]):
+			data.set_cell_type(cells[i], values[i])
+
+
+func _add_door_direction(data : WorldData, cell : int, value : int):
+	if not data.has_cell_meta(cell, data.CellMetaKeys.META_DOOR_DIRECTIONS):
+		data.set_cell_meta(cell, data.CellMetaKeys.META_DOOR_DIRECTIONS, Array())
+	if not data.get_cell_meta(cell, data.CellMetaKeys.META_DOOR_DIRECTIONS).has(value):
+		data.get_cell_meta(cell, data.CellMetaKeys.META_DOOR_DIRECTIONS).push_back(value)
+
+
+func _set_doorways_meta(data: WorldData, cells: Array, direction: int) -> void:
+	if cells.empty():
+		return 
+	
+	for cell in cells:
+		if data.get_cell_type(cell) == data.CellType.ROOM:
+			var room_data := data.get_cell_meta(cell, data.CellMetaKeys.META_ROOM_DATA) as RoomData
+			var corridor_direction = data.direction_inverse(direction)
+			room_data.set_doorway_cell(cell, corridor_direction)
+		elif data.get_cell_type(cell) == data.CellType.DOOR:
+			_add_door_direction(data, cell, direction)
+
+
+func _disconnect_room_walls_from_grid(data: WorldData, astar: AStar2D, room: RoomData) -> void:
+	var doorway_cells := room.get_all_doorway_cells()
+	for cell_index in room.cell_indexes:
+		for direction in data.Direction.DIRECTION_MAX:
+			if cell_index in doorway_cells:
+				continue
+			
+			var neighbour_index := data.get_neighbour_cell(cell_index, direction)
+			if neighbour_index != -1 and not neighbour_index in room.cell_indexes:
+				if astar.are_points_connected(cell_index, neighbour_index):
+					astar.disconnect_points(cell_index, neighbour_index)
+
+### -----------------------------------------------------------------------------------------------
+
+
+### Signal Callbacks ------------------------------------------------------------------------------
+
+### -----------------------------------------------------------------------------------------------

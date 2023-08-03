b8d5312c3ce4d50f69f7f10a8517a648ddf2a3a3
Daniel Queiroz Porto <danielvandalk@gmail.com>
2023-08-03 11:30:17 -0300

    Create staircase scenes

---
+++ scenes/objects/large_objects/staircases/level_staircase.gd
@@ -0,0 +1,52 @@
+# Write your doc string for this file here
+class_name LevelStaircase
+extends Spatial
+
+### Member Variables and Dependencies -------------------------------------------------------------
+#--- signals --------------------------------------------------------------------------------------
+
+#--- enums ----------------------------------------------------------------------------------------
+
+#--- constants ------------------------------------------------------------------------------------
+
+#--- public variables - order: export > normal var > onready --------------------------------------
+
+export var path_spawn_position := NodePath("PlayerSpawnPosition")
+export(WorldData.Direction) var facing_direction := WorldData.Direction.EAST \
+		setget _set_facing_direction
+
+#--- private variables - order: export > normal var > onready -------------------------------------
+
+onready var _spawn_position := get_node(path_spawn_position) as Position3D
+
+### -----------------------------------------------------------------------------------------------
+
+
+### Built-in Virtual Overrides --------------------------------------------------------------------
+
+### -----------------------------------------------------------------------------------------------
+
+
+### Public Methods --------------------------------------------------------------------------------
+
+### -----------------------------------------------------------------------------------------------
+
+
+### Private Methods -------------------------------------------------------------------------------
+
+func _set_facing_direction(value: int) -> void:
+	facing_direction = clamp(value, 0, WorldData.Direction.DIRECTION_MAX - 1)
+	var angle = _get_facing_rotation()
+	rotation.y = angle
+
+
+# Override this in child scenes, according to up or down staircase
+func _get_facing_rotation() -> float:
+	return 0.0
+
+### -----------------------------------------------------------------------------------------------
+
+
+### Signal Callbacks ------------------------------------------------------------------------------
+
+### -----------------------------------------------------------------------------------------------

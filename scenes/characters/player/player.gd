189884bac74f6281408dba5db1dbf70d8bc60f78
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2023-08-05 17:40:10 +0100

    Update player.gd

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -110,87 +110,66 @@ func _on_GrabCastDot_area_exited(area):
 
 func change_equipment_out(var is_mainhand : bool):
 	if(is_mainhand):
-		is_change_main_equip_in = false
-		mainhand_equipment_root.transform.origin.y = mainhand_orig_origin.y
-		mainhand_equipment_root.transform.origin.z = mainhand_orig_origin.z
+		
 		is_change_main_equip_out = true
 	else:
-		is_change_off_equip_in = false
-		offhand_equipment_root.transform.origin.y = offhand_orig_origin.y
-		offhand_equipment_root.transform.origin.z = offhand_orig_origin.z
+		
 		is_change_off_equip_out = true
 
 
 func change_maindhand_equipment_out():
-	if(is_change_main_equip_out):
-		var from = mainhand_equipment_root.transform.origin.y
-		mainhand_equipment_root.transform.origin.y = lerp(from, mainhand_orig_origin.y - 0.2, 0.3)
-
-		from = mainhand_equipment_root.transform.origin.z
-		mainhand_equipment_root.transform.origin.z = lerp(from, mainhand_orig_origin.z + 0.8, 0.3)
-
-		var d1 = mainhand_equipment_root.transform.origin.z - (mainhand_orig_origin.z + 0.8)
-
-		if d1 > -0.02:
-			is_change_main_equip_out = false
-			emit_signal("change_main_equipment_out_done")
+	is_change_main_equip_out = false
+	emit_signal("change_main_equipment_out_done")
 
 
 func change_offhhand_equipment_out():
-	if(is_change_off_equip_out):
-		var from = offhand_equipment_root.transform.origin.y
-		offhand_equipment_root.transform.origin.y = lerp(from, offhand_orig_origin.y - 0.25, 0.3)
-
-		from = offhand_equipment_root.transform.origin.z
-		offhand_equipment_root.transform.origin.z = lerp(from, offhand_orig_origin.z + 0.8, 0.3)
-
-		var d1 = offhand_equipment_root.transform.origin.z - (offhand_orig_origin.z + 0.8)
-
-		if d1 > -0.02:
-			is_change_off_equip_out = false
-			emit_signal("change_off_equipment_out_done")
+	is_change_off_equip_out = false
+	emit_signal("change_off_equipment_out_done")
 
 
 func change_equipment_in(var is_mainhand : bool):
-	if(is_mainhand):
-		is_change_main_equip_out = false
-		mainhand_equipment_root.transform.origin.y = mainhand_orig_origin.y - 0.2
-		mainhand_equipment_root.transform.origin.z = mainhand_orig_origin.z + 0.8
-		is_change_main_equip_in = true
-	else:
-		is_change_off_equip_out = false
-		offhand_equipment_root.transform.origin.y = offhand_orig_origin.y - 0.25
-		offhand_equipment_root.transform.origin.z = offhand_orig_origin.z + 0.8
-		is_change_off_equip_in = true
+	pass
+#	if(is_mainhand):
+#		is_change_main_equip_out = false
+#		mainhand_equipment_root.transform.origin.y = mainhand_orig_origin.y - 0.2
+#		mainhand_equipment_root.transform.origin.z = mainhand_orig_origin.z + 0.8
+#		is_change_main_equip_in = true
+#	else:
+#		is_change_off_equip_out = false
+#		offhand_equipment_root.transform.origin.y = offhand_orig_origin.y - 0.25
+#		offhand_equipment_root.transform.origin.z = offhand_orig_origin.z + 0.8
+#		is_change_off_equip_in = true
 
 
 func change_mainhand_equipment_in():
-	if(is_change_main_equip_in):
-		var from = mainhand_equipment_root.transform.origin.y
-		mainhand_equipment_root.transform.origin.y = lerp(from, mainhand_orig_origin.y, 0.3)
-
-		from = mainhand_equipment_root.transform.origin.z
-		mainhand_equipment_root.transform.origin.z = lerp(from, mainhand_orig_origin.z, 0.3)
-
-		var d1 = mainhand_equipment_root.transform.origin.z - (mainhand_orig_origin.z)
-
-		if d1 < 0.02:
-			mainhand_equipment_root.transform.origin.y = mainhand_orig_origin.y
-			mainhand_equipment_root.transform.origin.z = mainhand_orig_origin.z
-			is_change_main_equip_in = false
+	pass
+#	if(is_change_main_equip_in):
+#		var from = mainhand_equipment_root.transform.origin.y
+#		mainhand_equipment_root.transform.origin.y = lerp(from, mainhand_orig_origin.y, 0.3)
+#
+#		from = mainhand_equipment_root.transform.origin.z
+#		mainhand_equipment_root.transform.origin.z = lerp(from, mainhand_orig_origin.z, 0.3)
+#
+#		var d1 = mainhand_equipment_root.transform.origin.z - (mainhand_orig_origin.z)
+#
+#		if d1 < 0.02:
+#			mainhand_equipment_root.transform.origin.y = mainhand_orig_origin.y
+#			mainhand_equipment_root.transform.origin.z = mainhand_orig_origin.z
+#			is_change_main_equip_in = false
 
 
 func change_offhand_equipment_in():
-	if(is_change_off_equip_in):
-		var from = offhand_equipment_root.transform.origin.y
-		offhand_equipment_root.transform.origin.y = lerp(from, offhand_orig_origin.y, 0.3)
-
-		from = offhand_equipment_root.transform.origin.z
-		offhand_equipment_root.transform.origin.z = lerp(from, offhand_orig_origin.z, 0.3)
-
-		var d1 = offhand_equipment_root.transform.origin.z - (offhand_orig_origin.z)
-
-		if d1 < 0.02:
-			offhand_equipment_root.transform.origin.y = offhand_orig_origin.y
-			offhand_equipment_root.transform.origin.z = offhand_orig_origin.z
-			is_change_off_equip_in = false
+	pass
+#	if(is_change_off_equip_in):
+#		var from = offhand_equipment_root.transform.origin.y
+#		offhand_equipment_root.transform.origin.y = lerp(from, offhand_orig_origin.y, 0.3)
+#
+#		from = offhand_equipment_root.transform.origin.z
+#		offhand_equipment_root.transform.origin.z = lerp(from, offhand_orig_origin.z, 0.3)
+#
+#		var d1 = offhand_equipment_root.transform.origin.z - (offhand_orig_origin.z)
+#
+#		if d1 < 0.02:
+#			offhand_equipment_root.transform.origin.y = offhand_orig_origin.y
+#			offhand_equipment_root.transform.origin.z = offhand_orig_origin.z
+#			is_change_off_equip_in = false

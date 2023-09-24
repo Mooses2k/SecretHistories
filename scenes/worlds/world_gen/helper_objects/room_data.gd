f9238c0b74ae95e814ad06740eac23d6edcc1b64
Chris <Moses2k@gmail.com>
2023-09-23 19:06:22 -0700

    Candle/bandage physics tweaks, more room purposes, broken statues

--- scenes/worlds/world_gen/helper_objects/room_data.gd
+++ scenes/worlds/world_gen/helper_objects/room_data.gd
@@ -9,11 +9,43 @@ extends Resource
 
 enum OriginalPurpose {
 	EMPTY,
-	CRYPT,
-	FOUNTAIN,
+	CRYPT, # (1 door)
+	FOUNTAIN, # (2+ doors)
+	STATUE_GALLERY,
 	UP_STAIRCASE,
 	DOWN_STAIRCASE,
+#	ENTRY, # (2x2 max, 2+ doors) 
+#	ALCOVE, # (1x2 max, 1 door)
+#	CHAPEL,
+#	SHRINE, # (3x3 max)  
+#	MEDITATION_CHAMBER, # (2x2 max, 1 door, no wall lighting)
+#	MUSEUM,
+#	BESTIARY,
+#	RECORD_ROOM, # (3x3 min)
+#	WELL # (2x2 max)
 }
+
+#enum CurrentUse{
+#	UNUSED,
+#	KITCHEN, # (not well)
+#	PANTRY, # (3x3 max, 1 door, no wall lighting)
+#	STOREROOM, # (1 door)
+#	MESS_HALL, # (chooses biggest room)
+#	PRIVATE_DINING_ROOM, # (3x3 max, not well)
+#	GARBAGE_ROOM, # (no wall lighting) 
+#	STUDY, # (3x3 max, not well)
+#	LIBRARY, # (3x3 min, prefer records room)
+#	TEMPLE, # (3x3 min, prefer chapel)
+#	MEDITATION_CHAMBER, # (1 door, no wall lighting, prefer meditation_chamber) 
+#	LOUNGE, # (not well)
+#	BARRACKS, # (not well)
+#	PRIVATE_BEDROOM, # (3x3 max, not well)
+#	CLOSET, # (1 door, no wall lighting)
+#	GUARD_POST # (2x2 max, 2+ doors)
+#}
+#
+#export var percent_empty_original_rooms = 0.3
+#export var percent_unused_current_rooms = 0.6
 
 #--- constants ------------------------------------------------------------------------------------
 

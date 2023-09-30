866d1e5ec52c137e42647ca71cea13ff7ad488a3
crazyStewie <joelgbsilva@gmail.com>
2023-09-29 21:23:20 -0300

    AI improvements (Fix collision and vision, sound disabled)

--- core/ai/action_nodes/bt_look_at_target.gd
+++ core/ai/action_nodes/bt_look_at_target.gd
@@ -3,5 +3,8 @@ extends BT_Node
 
 
 func tick(state : CharacterState) -> int:
+	# This sets the target facing direction, and immediately exits.
+	# Note that the character may not be facing in the given direction yet
+	# When this node succeeds
 	state.face_direction = state.target_position - state.character.global_transform.origin
 	return Status.SUCCESS

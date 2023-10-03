39aabfad4228f9d168379d18c749d9e7fcc286bb
Daniel Queiroz Porto <danielvandalk@gmail.com>
2023-10-03 15:04:56 -0300

    Implement FloorLevelHandler without serialization and integrate it to game
    This allows for keeping each floor level and managing them. Only the
    current level will be in teh tree, the other loaded levels will be held
    as orphan nodes

--- scenes/game.gd
+++ scenes/game.gd
@@ -11,8 +11,10 @@ signal player_spawned(player)
 
 #--- constants ------------------------------------------------------------------------------------
 
-# Floor levels go from 0 to -4, so 5 possible floors per dungeon.
-const MIN_FLOOR_LEVEL = -4
+# Starts at -1 and goes down to -5 so that it's a but more intuitive to talk about the dungeon
+# floors and because suposedly, 0 is ground level.
+const HIGHEST_FLOOR_LEVEL = -1
+const LOWEST_FLOOR_LEVEL = -5
 
 #--- public variables - order: export > normal var > onready --------------------------------------
 
@@ -27,10 +29,11 @@ onready var ui_root : CanvasLayer = $GameUI
 onready var local_settings : SettingsClass = $"%LocalSettings"
 onready var world_environment: WorldEnvironment = $WorldEnvironment
 
-
 #--- private variables - order: export > normal var > onready -------------------------------------
 
-var _current_floor_level := 0
+var _current_floor_level := HIGHEST_FLOOR_LEVEL
+# Keys are floor level indices and values are FloorLevelHandler objects or null.
+var _loaded_levels := {}
 
 ### -----------------------------------------------------------------------------------------------
 
@@ -42,6 +45,9 @@ func _init():
 
 func _ready():
 	set_brightness()
+	for floor_index in range(HIGHEST_FLOOR_LEVEL, LOWEST_FLOOR_LEVEL-1, -1):
+		_loaded_levels[floor_index] = null
+	
 	yield(get_tree().create_timer(1), "timeout")
 	var _error = connect("level_loaded", self, "_on_first_level_loaded", [], CONNECT_ONESHOT)
 	load_level(start_level_scn)
@@ -58,10 +64,19 @@ func set_brightness():
 
 
 func load_level(packed : PackedScene):
-	level = packed.instance() as GameWorld
-	world_root.add_child(level)
-	level.create_world()
-	yield(level, "spawning_world_scenes_finished")
+	if _loaded_levels[_current_floor_level] == null:
+		level = packed.instance() as GameWorld
+		world_root.add_child(level)
+		level.create_world()
+		_loaded_levels[_current_floor_level] = FloorLevelHandler.new(level, _current_floor_level)
+		yield(level, "spawning_world_scenes_finished")
+	else:
+		var level_handler: FloorLevelHandler = _loaded_levels[_current_floor_level]
+		level = level_handler.get_level_instance()
+		world_root.add_child(level)
+		# this needs a yield because this function is called from within another yield
+		yield(get_tree(), "idle_frame")
+	
 	emit_signal("level_loaded", level)
 
 
@@ -109,41 +124,55 @@ func _on_first_level_loaded(_level : GameWorld):
 	_connect_staircase_events()
 
 
+func _handle_floor_change() -> void:
+	yield(_show_load_screen(), "completed")
+	_handle_floor_levels()
+	yield(load_level(start_level_scn), "completed")
+	_set_new_position_for_player()
+	LoadScene.emit_signal("scene_loaded")
+
+
+func _show_load_screen() -> void:
+	LoadScene.setup_loadscreen()
+	# Wait a bit so that the load screen is visible
+	yield(get_tree().create_timer(0.1), "timeout")
+
+
+func _handle_floor_levels() -> void:
+	world_root.remove_child(level)
+	level = null
+	for floor_level in _loaded_levels:
+		var level_handler := _loaded_levels[floor_level] as FloorLevelHandler
+		if level_handler != null:
+			level_handler.update_floor_data(_current_floor_level)
+
+
+func _set_new_position_for_player() -> void:
+	player.translation = level.get_player_spawn_position()
+	emit_signal("player_spawned", player)
+
+
 func _on_Events_up_staircase_used() -> void:
 	var old_value := _current_floor_level
-	_current_floor_level = int(min(0, _current_floor_level + 1))
+	_current_floor_level = int(min(HIGHEST_FLOOR_LEVEL, _current_floor_level + 1))
 	var has_changed := old_value != _current_floor_level
 	
 	if has_changed:
 		print("Floor level changed from: %s to: %s"%[old_value, _current_floor_level])
-		
-		LoadScene.setup_loadscreen()
-		level.queue_free()
-		level = null
-		yield(load_level(start_level_scn), "completed")
-		breakpoint
-	elif _current_floor_level == 0:
+		yield(_handle_floor_change(), "completed")
+	elif _current_floor_level == HIGHEST_FLOOR_LEVEL:
 		print("You're already at the top of the dungeon, can't go upper.")
 
 
 func _on_Events_down_staircase_used() -> void:
 	var old_value := _current_floor_level
-	_current_floor_level = int(max(MIN_FLOOR_LEVEL, _current_floor_level - 1))
+	_current_floor_level = int(max(LOWEST_FLOOR_LEVEL, _current_floor_level - 1))
 	var has_changed := old_value != _current_floor_level
 	
 	if has_changed:
 		print("Went down from: %s to: %s"%[old_value, _current_floor_level])
-		
-		LoadScene.setup_loadscreen()
-		# Wait a bit so that the load screen is visible
-		yield(get_tree().create_timer(0.1), "timeout")
-		level.queue_free()
-		level = null
-		yield(load_level(start_level_scn), "completed")
-		player.translation = level.get_player_spawn_position()
-		emit_signal("player_spawned", player)
-		LoadScene.emit_signal("scene_loaded")
-	elif _current_floor_level == MIN_FLOOR_LEVEL:
+		yield(_handle_floor_change(), "completed")
+	elif _current_floor_level == LOWEST_FLOOR_LEVEL:
 		print("You're already at the bottom of the dungeon, can't go lower.")
 
 ### -----------------------------------------------------------------------------------------------

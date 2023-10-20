0817be0cd4004c40f3c5f2f732023ea92ba99dc8
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2023-10-20 17:00:08 +0100

    New changes for animations_player_test

--- scenes/characters/player/player_animations_test.gd
+++ scenes/characters/player/player_animations_test.gd
@@ -6,9 +6,30 @@ export(String,
 "Lee-metford_rifle", "Double-barrel_sawed_shotgun", 
 "Double-barrel_shotgun", "Martini_henry_rifle") var current_weapon = "Webley" setget change_gun
 #export var river_settings: bool setget set_river_settings
-export var weapon_status : bool
+export (String, "Idle", "ADS", "Reload") var weapon_status = "Idle"
+
+var spawned_weapon
+
+onready var inventory = $"../Inventory"
+onready var main_hand_equipment_root = $"../MainHandEquipmentRoot"
 
 
 func change_gun(value):
 	current_weapon = value
-	print("Changed Gun Is: ", str(value))
+	
+	if value == "Webley":
+		spawned_weapon = preload("res://scenes/objects/pickable_items/equipment/ranged/webley_revolver/webley.tscn").instance()
+	elif value == "Khyber_pass_martini":
+		spawned_weapon = preload("res://scenes/objects/pickable_items/equipment/ranged/khyber_pass_martini/khyber_pass_martini.tscn").instance()
+	elif value == "Lee-metford_rifle":
+		spawned_weapon = preload("res://scenes/objects/pickable_items/equipment/ranged/lee-metford_rifle/lee-metford_rifle.tscn").instance()
+	elif value == "Double-barrel_sawed_shotgun":
+		spawned_weapon = preload("res://scenes/objects/pickable_items/equipment/ranged/double-barrel_sawed_shotgun/sawed-off_shotgun.tscn").instance()
+	elif value == "Double-barrel_shotgun":
+		spawned_weapon = preload("res://scenes/objects/pickable_items/equipment/ranged/double-barrel_shotgun/double-barrel_shotgun.tscn").instance()
+	elif value == "Martini_henry_rifle":
+		spawned_weapon = preload("res://scenes/objects/pickable_items/equipment/ranged/martini_henry_rifle/martini_henry_rifle.tscn").instance()
+		
+	
+	$"../Inventory".add_item(spawned_weapon)
+	$"%MainHandEquipmentRoot".add_child(spawned_weapon)

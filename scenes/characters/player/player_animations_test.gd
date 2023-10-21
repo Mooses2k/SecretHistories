e15d1352dc60f8305b094e3f95a9407663c5fb09
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2023-10-21 14:49:51 +0100

    Tool ADS changes beginning

--- scenes/characters/player/player_animations_test.gd
+++ scenes/characters/player/player_animations_test.gd
@@ -7,13 +7,16 @@ export(String,
 "Double-barrel_shotgun", "Martini_henry_rifle") var current_weapon = "Webley" setget change_gun
 #export var river_settings: bool setget set_river_settings
 export (String, "Idle", "ADS", "Reload") var weapon_status = "Idle" setget set_weapon_state
-
+export var _cam_path : NodePath
 var spawned_weapon
 
 onready var inventory = $"../Inventory"
 onready var main_hand_equipment_root = $"../MainHandEquipmentRoot"
 onready var animation_tree = $"%AnimationTree"
 
+
+onready var arm_position = $"%MainCharOnlyArmsGameRig".translation
+onready var _camera : ShakeCamera = get_node(_cam_path) as Camera
 
 func change_gun(value):
 	current_weapon = value
@@ -49,22 +52,46 @@ func set_weapon_state(value):
 		if available_weapons is EquipmentItem:
 			if value == "Idle":
 				if available_weapons.item_size == 0:
-					print("Current gun is small")
 					$"%AnimationTree".set("parameters/Hand_Transition/current", 0)
 					$"%AnimationTree".set("parameters/OffHand_MainHand_Blend/blend_amount", 0)
 					$"%AnimationTree".set("parameters/Weapon_states/current", 2)
 				else:
-					print("Current gun is big")
 					$"%AnimationTree".set("parameters/Hand_Transition/current", 0)
 					$"%AnimationTree".set("parameters/OffHand_MainHand_Blend/blend_amount", 0)
 					$"%AnimationTree".set("parameters/Weapon_states/current", 1)
-			else:
-				pass
+			elif value == "ADS":
+				operation_tween(
+				available_weapons.hold_position, "rotation", 
+				available_weapons.hold_position.rotation, 
+				available_weapons.ads_hold_rotation, 0.1
+			)
+				if available_weapons.item_size == 0:
+					operation_tween($"%AnimationTree", 
+					"parameters/SmallAds/blend_amount", 
+					$"%AnimationTree".get("parameters/SmallAds/blend_amount"), 
+					1.0, 
+					0.15
+					)
+					$"../FPSCamera".fov = lerp($"../FPSCamera".fov, 65, 0.1)
+					adjust_arm(Vector3(-0.086, -1.558, 0.294))
+				else:
+					operation_tween($"%AnimationTree",
+					"parameters/MediumAds/blend_amount",
+					$"%AnimationTree".get("parameters/MediumAds/blend_amount"), 
+					1.0, 
+					0.15
+					)
+					$"../FPSCamera".fov = lerp($"../FPSCamera".fov, 60, 0.1)
+					adjust_arm(Vector3(-0.03, -1.635, 0.218))
 
-#
-#			animation_tree.set("parameters/Hand_Transition/current", 0)
-#			animation_tree.set("parameters/Weapon_states/current", 2)
-#		else:
-#			animation_tree.set("parameters/Hand_Transition/current", 0)
-#			animation_tree.set("parameters/OffHand_MainHand_Blend/blend_amount", 0)
-#			animation_tree.set("parameters/Weapon_states/current", 1)
+
+func operation_tween(object : Object, method, tweening_from, tweening_to, duration):
+	var tweener = Tween.new() as Tween
+	tweener.interpolate_property(object, method, tweening_from, tweening_to, duration, Tween.TRANS_LINEAR)
+	add_child(tweener)
+	tweener.start()
+
+
+func adjust_arm(final_position):
+	$"%ADSTween".interpolate_property($"%MainCharOnlyArmsGameRig", "translation", $"%MainCharOnlyArmsGameRig".translation, final_position, 0.15)
+	$"%ADSTween".start()

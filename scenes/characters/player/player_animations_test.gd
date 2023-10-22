5d6df46279a8e5a61720c88b51eb46e02e0659ac
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2023-10-22 17:20:11 +0100

    Now able to save changes

--- scenes/characters/player/player_animations_test.gd
+++ scenes/characters/player/player_animations_test.gd
@@ -13,16 +13,17 @@ export (Vector3) var adjust_weapon_dial setget adjust_weapon_position
 export (Vector3) var adjust_weapon_rotation_dial setget adjust_weapon_rotation
 export (Vector3) var weapon_default_position setget set_default_position
 export (Vector3) var weapon_default_rotation setget set_default_rotation
-#export (bool) var save_changes_to_gun setget 
+export (bool) var save_all_changes_to_gun = true setget save_changes_to_gun
 
 var spawned_weapon
+var spawned_weapon_path : String
 var is_doing_ads : bool = false
 
 onready var inventory = $"../Inventory"
 onready var main_hand_equipment_root = $"../MainHandEquipmentRoot"
 onready var animation_tree = $"%AnimationTree"
 onready var arm_position = $"%MainCharOnlyArmsGameRig".translation
-#
+
 #ResourceSaver.save("res://path_to_modified_scene.tscn", scene_instance)
 
 func change_gun(value):
@@ -32,22 +33,27 @@ func change_gun(value):
 		return
 	if value == "Webley":
 		spawned_weapon = preload("res://scenes/objects/pickable_items/equipment/ranged/webley_revolver/webley.tscn").instance()
+		spawned_weapon_path = "res://scenes/objects/pickable_items/equipment/ranged/webley_revolver/webley.tscn"
 		
 	elif value == "Khyber_pass_martini":
 		spawned_weapon = preload("res://scenes/objects/pickable_items/equipment/ranged/khyber_pass_martini/khyber_pass_martini.tscn").instance()
+		spawned_weapon_path = "res://scenes/objects/pickable_items/equipment/ranged/khyber_pass_martini/khyber_pass_martini.tscn"
 		
 	elif value == "Lee-metford_rifle":
 		spawned_weapon = preload("res://scenes/objects/pickable_items/equipment/ranged/lee-metford_rifle/lee-metford_rifle.tscn").instance()
+		spawned_weapon_path = "res://scenes/objects/pickable_items/equipment/ranged/lee-metford_rifle/lee-metford_rifle.tscn"
 		
 	elif value == "Double-barrel_sawed_shotgun":
 		spawned_weapon = preload("res://scenes/objects/pickable_items/equipment/ranged/double-barrel_sawed_shotgun/sawed-off_shotgun.tscn").instance()
+		spawned_weapon_path = "res://scenes/objects/pickable_items/equipment/ranged/double-barrel_sawed_shotgun/sawed-off_shotgun.tscn"
 		
 	elif value == "Double-barrel_shotgun":
 		spawned_weapon = preload("res://scenes/objects/pickable_items/equipment/ranged/double-barrel_shotgun/double-barrel_shotgun.tscn").instance()
+		spawned_weapon_path = "res://scenes/objects/pickable_items/equipment/ranged/double-barrel_shotgun/double-barrel_shotgun.tscn"
 		
 	elif value == "Martini_henry_rifle":
 		spawned_weapon = preload("res://scenes/objects/pickable_items/equipment/ranged/martini_henry_rifle/martini_henry_rifle.tscn").instance()
-		
+		spawned_weapon_path = "res://scenes/objects/pickable_items/equipment/ranged/martini_henry_rifle/martini_henry_rifle.tscn"
 	
 	for available_weapons in $"%MainHandEquipmentRoot".get_children():
 		available_weapons.queue_free()
@@ -107,9 +113,20 @@ func set_default_rotation(value):
 			available_weapons.transform = available_weapons.get_hold_transform()
 
 
-func save_changes_to_gun():
-	pass
-
+func save_changes_to_gun(value):
+	if not Engine.editor_hint:
+		return
+	
+	var new_gun_changes = GunItem.new()
+	new_gun_changes.ads_parameters.ads_hold_position = adjust_weapon_dial
+	new_gun_changes.ads_parameters.ads_rotation_position = adjust_weapon_rotation_dial
+	new_gun_changes.ads_parameters.ads_reset_position = weapon_default_position
+	new_gun_changes.ads_parameters.ads_reset_rotation = weapon_default_rotation
+	
+	save_all_changes_to_gun = true
+	
+	ResourceSaver.save(spawned_weapon_path, new_gun_changes)
+	print("Saved weapon", new_gun_changes)
 
 func reset_animation_tree(value):
 	if not Engine.editor_hint:

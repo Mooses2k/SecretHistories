89c278a766d2632e5214e36bdd443c68cba1298f
Chris <Moses2k@gmail.com>
2023-11-22 05:11:15 -0800

    Sugardrenaline's PSX shader fix, fix grab throw large object, demoIK #

--- scenes/characters/cultists/demo_ik.gd
+++ scenes/characters/cultists/demo_ik.gd
@@ -1,5 +1,8 @@
 extends Node
 
+### Purpose of this is to stop the cultist in his tracks, then turn head to look at camera.
+### This is to provide for a cool scene for the demo trailer.
+
 
 enum DemoState {
 	NONE,

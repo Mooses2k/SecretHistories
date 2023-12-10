5e087babc662f48fe3f444caf88db49a178bce35
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2023-12-10 18:55:45 +0100

    Added ability to edit shotgun shell impulses

--- scenes/objects/pickable_items/equipment/ranged/double-barrel_sawed_shotgun/shell_reload_effects.gd
+++ scenes/objects/pickable_items/equipment/ranged/double-barrel_sawed_shotgun/shell_reload_effects.gd
@@ -1,47 +1,13 @@
 tool
 extends Node
 
-export var expell_shells = true setget expell_shotgun_shells_tools
-export var clear_shotgun_shells = true setget clear_shotgun_shells
+export var impulse_position : Vector3 = Vector3(0, 0.2, 0.1)
+export var impulse_value : Vector3 = Vector3(0, 0.3, 0.2)
 
 var shotgun_shell = preload("res://scenes/objects/pickable_items/tiny/ammo/shotgun_shells/12-gauge_shotgun_shell.tscn")
 
 onready var shell_position_1 = $"%ShellPosition1"
 onready var shell_position_2 = $"%ShellPosition2"
-
-
-func clear_shotgun_shells(value):
-	clear_shotgun_shells = value
-	if not Engine.editor_hint:
-		return
-	for current_first_shells in $"%ShellPosition1".get_children():
-		current_first_shells.queue_free()
-		
-	for current_first_shells in $"%ShellPosition2".get_children():
-		current_first_shells.queue_free()
-
-
-func expell_shotgun_shells_tools(value):
-	expell_shells = true
-	if not Engine.editor_hint:
-		return
-	var first_shell = shotgun_shell.instance() as RigidBody
-	var second_shell = shotgun_shell.instance() as RigidBody
-
-	for current_first_shells in $"%ShellPosition1".get_children():
-		current_first_shells.queue_free()
-		
-	for current_first_shells in $"%ShellPosition2".get_children():
-		current_first_shells.queue_free()
-	
-	first_shell.rotation_degrees.z = 100.695
-	second_shell.rotation_degrees.z = 100.695
-	$"%ShellPosition1".add_child(first_shell)
-	$"%ShellPosition2".add_child(second_shell)
-	
-#	first_shell.apply_impulse(Vector3(0, 0, 0), Vector3(0, 90, 0) * 90)
-#	second_shell.apply_impulse(Vector3(0, 0, 0), Vector3(0, 90, 0) * 90)
-
 
 func expell_shotgun_shells():
 	print("Expelling shotgun shells")
@@ -60,8 +26,8 @@ func expell_shotgun_shells():
 	world_scene.add_child(first_shell)
 	world_scene.add_child(second_shell)
 	
-	first_shell.apply_impulse(Vector3(0, 0.2, 0.1), Vector3(0, 0.3, 0.2) )
-	second_shell.apply_impulse(Vector3(0, 0.2, 0.1), Vector3(0, 0.3, 0.2) )
+	first_shell.apply_impulse(impulse_position, impulse_value)
+	second_shell.apply_impulse(impulse_position, impulse_value)
 
 
 func player_add_shell():
@@ -69,7 +35,3 @@ func player_add_shell():
 
 func player_clear_shell():
 	owner.owner_character.player_gun_reload_shells.clear_shotgun_shells()
-
-func delete_clear_shell():
-	pass
-

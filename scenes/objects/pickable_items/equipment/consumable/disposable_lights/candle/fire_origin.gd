bd0383c87aa7d21911a57454c60b8656c8f7ac34
crazyStewie <joelgbsilva@gmail.com>
2023-12-14 21:23:02 -0300

    Implemented auto turn off toppled over lights

--- scenes/objects/pickable_items/equipment/consumable/disposable_lights/candle/fire_origin.gd
+++ scenes/objects/pickable_items/equipment/consumable/disposable_lights/candle/fire_origin.gd
@@ -1,5 +1,25 @@
 extends Position3D
 
+export var threshold_angle_degrees = 60
+onready var threshold_factor = cos(deg2rad(threshold_angle_degrees))
+onready var fire: Spatial = $Fire
+onready var light: Spatial = $Fire/Light
 
 func _physics_process(delta):
 	global_transform.basis = Basis.IDENTITY
+	handle_toppled_light()
+
+func handle_toppled_light():
+	if owner is TorchItem:
+		return
+	if owner.global_transform.basis.y.normalized().y < threshold_factor:
+		if owner is PickableItem and owner.item_state == GlobalConsts.ItemState.EQUIPPED:
+			return
+		if owner.has_method("unlight"):
+			owner.unlight()
+		# TODO Implement a better way for light sources without unlight method
+		else:
+			if is_instance_valid(fire):
+				fire.visible = false
+			if is_instance_valid(light):
+				light.visible = false
---
+++ scenes/objects/pickable_items/equipment/tool/light-sources/lantern_light.gd
@@ -0,0 +1,15 @@
+extends Spatial
+
+export var threshold_angle_degrees = 60
+onready var threshold_factor = cos(deg2rad(threshold_angle_degrees))
+
+# This code is duplicated from "res://scenes/objects/pickable_items/equipment/consumable/disposable_lights/candle/fire_origin.gd"
+func handle_toppled_light():
+	if owner.global_transform.basis.y.normalized().y < threshold_factor:
+		if owner.item_state == GlobalConsts.ItemState.EQUIPPED:
+			return
+		if owner.has_method("unlight"):
+			owner.unlight()
+
+func _physics_process(delta: float) -> void:
+	handle_toppled_light()

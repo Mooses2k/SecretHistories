cb7fd498afb63cba34336a407d38061cf3dfb404
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2023-12-16 23:20:13 +0100

    More changes to reloading

--- scenes/objects/pickable_items/equipment/ranged/webley_revolver/shell_reload_effects.gd
+++ scenes/objects/pickable_items/equipment/ranged/webley_revolver/shell_reload_effects.gd
@@ -5,7 +5,10 @@ export var impulse_value : Vector3 = Vector3(0, 0.3, 0.2)
 
 var dropped_bullet_shell = preload("res://scenes/objects/pickable_items/tiny/ammo/webley/webley_casing.tscn")
 var bullet_shells = preload("res://scenes/objects/pickable_items/tiny/ammo/webley/webley_round.tscn")
+
+var all_shell_positions : Array = []
 var all_bullet_shells : Array = []
+
 
 onready var bullet_position_1 = $"%BulletPosition"
 onready var bullet_position_2 = $"%BulletPosition2"
@@ -14,11 +17,17 @@ onready var bullet_position_4 = $"%BulletPosition4"
 onready var bullet_position_5 = $"%BulletPosition5"
 onready var bullet_position_6 = $"%BulletPosition6"
 
+func _ready():
+	all_shell_positions.append(bullet_position_1)
+	all_shell_positions.append(bullet_position_2)
+	all_shell_positions.append(bullet_position_3)
+	all_shell_positions.append(bullet_position_4)
+	all_shell_positions.append(bullet_position_5)
+	all_shell_positions.append(bullet_position_6)
+
 
 func expell_shells():
 	all_bullet_shells.clear()
-	print("Expelling shotgun shells")
-	
 	var first_shell = dropped_bullet_shell.instance() as RigidBody
 	var second_shell = dropped_bullet_shell.instance() as RigidBody
 	var third_shell = dropped_bullet_shell.instance() as RigidBody
@@ -60,7 +69,22 @@ func expell_shells():
 
 
 func player_add_shell():
-	owner.owner_character.player_gun_reload_shells.spawn_bullet_shells(bullet_shells, Vector3(), Vector3())
+	owner.owner_character.player_gun_reload_shells.spawn_bullet_shells(bullet_shells, Vector3(0.014, -0.001, -0.003), Vector3(1.372, 6.429, 0.474))
+	add_shells_to_slot()
 
 func player_clear_shell():
 	owner.owner_character.player_gun_reload_shells.clear_shotgun_shells()
+
+
+func add_shells_to_slot():
+	var added_new_shells = bullet_shells.instance()
+	for shells_positions in all_shell_positions:
+		if shells_positions.get_child_count() < 1:
+			shells_positions.add_child(added_new_shells)
+			added_new_shells.translation = Vector3(0.011, 0, 0)
+
+func clear_all_slots():
+	var added_new_shells = bullet_shells.instance()
+	for shells_positions in all_shell_positions:
+		for bullet_shells in shells_positions.get_children():
+			bullet_shells.queue_free()

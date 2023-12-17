8dc482788e94b9afa912cdac91a8baf5815d74f6
Ayomide Abdulazeez <109722734+Ayomideabdulazeez@users.noreply.github.com>
2023-12-17 20:50:43 +0100

    Made it visible when inserting shotgun shells

--- scenes/objects/pickable_items/equipment/ranged/double-barrel_sawed_shotgun/shell_reload_effects.gd
+++ scenes/objects/pickable_items/equipment/ranged/double-barrel_sawed_shotgun/shell_reload_effects.gd
@@ -8,7 +8,12 @@ var shotgun_shell = preload("res://scenes/objects/pickable_items/tiny/ammo/shotg
 onready var shell_position_1 = $"%ShellPosition1"
 onready var shell_position_2 = $"%ShellPosition2"
 
+var all_shell_positions : Array = []
+
 func expell_shotgun_shells():
+	all_shell_positions.append(shell_position_1)
+	all_shell_positions.append(shell_position_2)
+	
 	print("Expelling shotgun shells")
 	var first_shell = shotgun_shell.instance() as RigidBody
 	var second_shell = shotgun_shell.instance() as RigidBody
@@ -33,6 +38,22 @@ func expell_shotgun_shells():
 	
 	first_shell.apply_impulse(impulse_position, impulse_value)
 	second_shell.apply_impulse(impulse_position, impulse_value)
+
+
+func add_shells_to_slot():
+	var added_new_shells = shotgun_shell.instance()
+	added_new_shells.translation = Vector3(0.001, -0.035, 0)
+	added_new_shells.rotation_degrees = Vector3(0, 0, -82.59)
+	for shells_positions in all_shell_positions:
+		if shells_positions.get_child_count() < 1:
+			shells_positions.add_child(added_new_shells)
+
+
+func clear_all_slots():
+	var added_new_shells = shotgun_shell.instance()
+	for shells_positions in all_shell_positions:
+		for bullet_shells in shells_positions.get_children():
+			bullet_shells.queue_free()
 
 
 func player_add_shell():

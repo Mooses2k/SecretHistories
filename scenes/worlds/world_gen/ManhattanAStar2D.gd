12fb9392cace82076e2593dbfc379005c670654d
crazy_stewie <30158085+crazyStewie@users.noreply.github.com>
2024-01-23 21:23:40 -0300

    Commented ManhattanAStar2D

--- scenes/worlds/world_gen/ManhattanAStar2D.gd
+++ scenes/worlds/world_gen/ManhattanAStar2D.gd
@@ -1,11 +1,13 @@
 class_name ManhattanAStar2D
 extends AStar2D
 
-
+# Computes the true manhattan distance between 2 points
 func _compute_cost(from_id: int, to_id: int) -> float:
 	return get_manhattan_distance(get_point_position(from_id), get_point_position(to_id))
 
-
+# Computes a biased manhattan distance between 2 points, giving less weight
+# to longer edges. This makes it so the algorithm will prefer to move along
+# shorter edges first, and avoid zig zags
 func _estimate_cost(from_id: int, to_id: int) -> float:
 	return get_biased_manhattan_distance(get_point_position(from_id), get_point_position(to_id))
 

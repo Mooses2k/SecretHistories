f2d36aa3700d2b7d022a34f95e7d61200f8c0572
Vinicius Antonio da Silva <70033167+Visnicio@users.noreply.github.com>
2025-03-29 20:55:04 -0300

    Migrated audio files from cultists to AudioLibrary

--- scenes/characters/character_audio.gd
+++ scenes/characters/character_audio.gd
@@ -34,23 +34,11 @@ enum SpeechType {
 	COMET
 }
 
-var _idle_sounds : Array = []
-var _alert_sounds : Array = []
-var _detection_sounds : Array = []
-var _ambush_sounds : Array = []
-var _chase_sounds : Array = []
-var _fight_sounds : Array = []
 var _reload_sounds : Array = []
 var _out_of_ammo_sounds : Array = []
-var _flee_sounds : Array = []
 var _dialog_q_sounds : Array = []
 var _dialog_a_sounds : Array = []
 var _dialog_sequence_sounds : Array = []
-var _surprised_sounds : Array = []
-var _fire_sounds : Array = []
-var _snake_sounds : Array = []
-var _bomb_sounds : Array = []
-var _comet_sounds : Array = []
 
 var _current_sound_dir : String = ""
 
@@ -64,90 +52,11 @@ var last_speech_type   # Tracked to avoid interrupting self to say same type of 
 
 func _ready():
 	# Movement audio	
-	load_sounds("resources/sounds/breathing/breathe", 1)
-	load_sounds("resources/sounds/jumping_landing/landing", 2)
+	#load_sounds("resources/sounds/breathing/breathe", 1)
+	#load_sounds("resources/sounds/jumping_landing/landing", 2)
 
 	choose_voice()   # Choose one from the appropriate voices for this character
 	pitch_alter_voice()   # Randomly alter pitch of this character's voice up or down some
-
-
-func load_sounds(sound_dir, type : int) -> void:
-	if sound_dir == "":
-		return
-
-	if _current_sound_dir == sound_dir:
-		return
-
-	_current_sound_dir = sound_dir
-
-	if sound_dir.ends_with("/"):
-		sound_dir.erase(sound_dir.length() - 1, 1)
-
-	if !sound_dir.begins_with("res://"):
-		sound_dir = "res://" + sound_dir
-	
-	
-	var snd_dir = DirAccess.open(sound_dir)
-	
-	if not is_instance_valid(snd_dir):
-		push_error("Unable to open sound directory :", sound_dir)
-		return
-	
-	snd_dir.include_hidden = false
-	snd_dir.include_navigational = false
-	snd_dir.list_dir_begin() # TODOConverter3To4 fill missing arguments https://github.com/godotengine/godot/pull/40547
-
-	var sound = snd_dir.get_next()
-	while sound != "":
-		if not sound.ends_with(".import") and (sound.ends_with(".wav") or sound.ends_with(".ogg") or sound.ends_with(".mp3")):
-			match type:
-				# Movement
-#				0:
-					# old footstep
-				1:
-					if "in" in sound:
-						_clamber_sounds["in"].append(load(sound_dir + "/" + sound))
-					elif "out" in sound:
-						_clamber_sounds["out"].append(load(sound_dir + "/" + sound))
-				2:
-					_landing_sounds.append(load(sound_dir + "/" + sound))
-				# Speech
-				13:
-					_idle_sounds.append(load(sound_dir + "/" + sound))
-				14:
-					_alert_sounds.append(load(sound_dir + "/" + sound))
-				15:
-					_detection_sounds.append(load(sound_dir + "/" + sound))
-				16:
-					_ambush_sounds.append(load(sound_dir + "/" + sound))
-				17:
-					_chase_sounds.append(load(sound_dir + "/" + sound))
-				18:
-					_fight_sounds.append(load(sound_dir + "/" + sound))
-				19:
-					_reload_sounds.append(load(sound_dir + "/" + sound))
-				20:
-					_out_of_ammo_sounds.append(load(sound_dir + "/" + sound))
-				21:
-					_flee_sounds.append(load(sound_dir + "/" + sound))
-				22:
-					_dialog_q_sounds.append(load(sound_dir + "/" + sound))
-				23:
-					_dialog_a_sounds.append(load(sound_dir + "/" + sound))
-				24:
-					_dialog_sequence_sounds.append(load(sound_dir + "/" + sound))
-				25:
-					_surprised_sounds.append(load(sound_dir + "/" + sound))
-				26:
-					_fire_sounds.append(load(sound_dir + "/" + sound))
-				27:
-					_snake_sounds.append(load(sound_dir + "/" + sound))
-				28:
-					_bomb_sounds.append(load(sound_dir + "/" + sound))
-				29:
-					_comet_sounds.append(load(sound_dir + "/" + sound))
-					
-		sound = snd_dir.get_next()
 
 
 ### Speech
@@ -156,6 +65,8 @@ func load_sounds(sound_dir, type : int) -> void:
 func choose_voice():
 	if owner is Cultist:   # Later: Neophyte, later more types
 	
+	
+		# TODO audio library currently doesnt support multiple voices
 		var choose = randi() % 2
 		match choose:
 			0:
@@ -165,23 +76,23 @@ func choose_voice():
 		
 		# Speech audio - these should eventually be moved to each enemy's script or character audio
 		# and the paths adjusted to the correct voice
-		load_sounds(character_voice_path + "idle", 13)
-		load_sounds(character_voice_path + "alert", 14)
-		load_sounds(character_voice_path + "detection", 15)
-		load_sounds(character_voice_path + "ambush", 16)
-		load_sounds(character_voice_path + "chase", 17)
-		load_sounds(character_voice_path + "fight", 18)
-		load_sounds(character_voice_path + "reload", 19)
-		load_sounds(character_voice_path + "out_of_ammo", 20)
-		load_sounds(character_voice_path + "flee", 21)
-		load_sounds(character_voice_path + "dialog_q", 22)
-		load_sounds(character_voice_path + "dialog_a", 23)
-		load_sounds(character_voice_path + "dialog_sequence", 24)
-		load_sounds(character_voice_path + "surprised", 25)
-		load_sounds(character_voice_path + "fire", 26)
-		load_sounds(character_voice_path + "snake", 27)
-		load_sounds(character_voice_path + "bomb", 28)
-		load_sounds(character_voice_path + "comet", 29)
+		#load_sounds(character_voice_path + "idle", 13)
+		#load_sounds(character_voice_path + "alert", 14)
+		#load_sounds(character_voice_path + "detection", 15)
+		#load_sounds(character_voice_path + "ambush", 16)
+		#load_sounds(character_voice_path + "chase", 17)
+		#load_sounds(character_voice_path + "fight", 18)
+		#load_sounds(character_voice_path + "reload", 19)
+		#load_sounds(character_voice_path + "out_of_ammo", 20)
+		#load_sounds(character_voice_path + "flee", 21)
+		#load_sounds(character_voice_path + "dialog_q", 22)
+		#load_sounds(character_voice_path + "dialog_a", 23)
+		#load_sounds(character_voice_path + "dialog_sequence", 24)
+		#load_sounds(character_voice_path + "surprised", 25)
+		#load_sounds(character_voice_path + "fire", 26)
+		#load_sounds(character_voice_path + "snake", 27)
+		#load_sounds(character_voice_path + "bomb", 28)
+		#load_sounds(character_voice_path + "comet", 29)
 
 
 func pitch_alter_voice():
@@ -194,8 +105,8 @@ func play_idle_sound():
 #		print("Sound already playing (idle called this)")
 		return
 	# Tracked to avoid repeating the same line
-	_idle_sounds.shuffle()
-	speech_audio.stream = _idle_sounds.front()
+	var sound = AudioLibrary.get_voicelines(AudioLibrary.CULTIST_VOICE_TYPE.IDLE).pick_random()
+	speech_audio.stream = sound
 	# Don't replay the last line
 	if last_speech_line == speech_audio.stream:
 		return
@@ -211,14 +122,14 @@ func play_alert_sound():
 		if speech_audio.is_playing() == true:
 #			print("Sounds already playing (alert called this)")
 			return
-	_alert_sounds.shuffle()
-	speech_audio.stream = _alert_sounds.front()
+	var sound: AudioStream = AudioLibrary.get_voicelines(AudioLibrary.CULTIST_VOICE_TYPE.ALERT).pick_random()
+	speech_audio.stream = sound
 	if last_speech_line == speech_audio.stream:   # This is not working to stop duplicate lines =/
 		return 
 	last_speech_line = speech_audio.stream   # Tracked to avoid repeating the same line
 	last_speech_type = SpeechType.ALERT
 	speech_audio.play()
-	print("Played alert sound")
+	print_debug("Played alert sound")
 	
 
 func get_player() -> Player:
@@ -233,11 +144,11 @@ func play_detection_sound() -> void:
 #			print("Sounds already playing (detection called this)")
 			return
 	if is_instance_valid(get_player()) and get_player().inventory.bulky_equipment is ShardOfTheComet:
-		_comet_sounds.shuffle()
-		speech_audio.stream = _comet_sounds.front()
+		var sound = AudioLibrary.get_voicelines(AudioLibrary.CULTIST_VOICE_TYPE.COMET).pick_random()
+		speech_audio.stream = sound
 	else:
-		_detection_sounds.shuffle()
-		speech_audio.stream = _detection_sounds.front()
+		var sound = AudioLibrary.get_voicelines(AudioLibrary.CULTIST_VOICE_TYPE.DETECTION).pick_random()
+		speech_audio.stream = sound
 	if last_speech_line == speech_audio.stream:
 		return 
 	last_speech_line = speech_audio.stream   # Tracked to avoid repeating the same line
@@ -247,23 +158,11 @@ func play_detection_sound() -> void:
 
 
 func play_ambush_sound():
-	_ambush_sounds.shuffle()
-	speech_audio.stream = _ambush_sounds.front()
-	if last_speech_line == speech_audio.stream:
-		return 
-	last_speech_line = speech_audio.stream   # Tracked to avoid repeating the same line
-	last_speech_type = SpeechType.AMBUSH
-	speech_audio.play()
+	_play_sound_from_library(AudioLibrary.CULTIST_VOICE_TYPE.AMBUSH)
 
 
 func play_chase_sound():
-	_chase_sounds.shuffle()
-	speech_audio.stream = _chase_sounds.front()
-	if last_speech_line == speech_audio.stream:
-		return 
-	last_speech_line = speech_audio.stream   # Tracked to avoid repeating the same line
-	last_speech_type = SpeechType.CHASE
-	speech_audio.play()
+	_play_sound_from_library(AudioLibrary.CULTIST_VOICE_TYPE.CHASE)
 
 
 func play_fight_sound():
@@ -271,8 +170,8 @@ func play_fight_sound():
 		# This means he doesn't interrupt itself - for detection lines, they should, but not idles, reloads, etc
 		if speech_audio.is_playing() == true:
 			return
-	_fight_sounds.shuffle()
-	speech_audio.stream = _fight_sounds.front()
+	var sound = AudioLibrary.get_voicelines(AudioLibrary.CULTIST_VOICE_TYPE.FIGHT).pick_random()
+	speech_audio.stream = sound
 	if last_speech_line == speech_audio.stream:
 		return 
 	last_speech_line = speech_audio.stream   # Tracked to avoid repeating the same line
@@ -295,13 +194,7 @@ func play_reload_sound():
 
 
 func play_flee_sound():
-	_flee_sounds.shuffle()
-	speech_audio.stream = _flee_sounds.front()
-	if last_speech_line == speech_audio.stream:
-		return 
-	last_speech_line = speech_audio.stream   # Tracked to avoid repeating the same line
-	last_speech_type = SpeechType.FLEE
-	speech_audio.play()
+	_play_sound_from_library(AudioLibrary.CULTIST_VOICE_TYPE.FLEE)
 
 
 # Needs logic for categories of question lines
@@ -338,53 +231,23 @@ func play_dialog_sequence_sound():
 
 
 func play_surprised_sound():
-	_surprised_sounds.shuffle()
-	speech_audio.stream = _surprised_sounds.front()
-	if last_speech_line == speech_audio.stream:
-		return 
-	last_speech_line = speech_audio.stream   # Tracked to avoid repeating the same line
-	last_speech_type = SpeechType.SURPRISED
-	speech_audio.play()
+	_play_sound_from_library(AudioLibrary.CULTIST_VOICE_TYPE.SURPRISED)
 
 
 func play_fire_sound():
-	_fire_sounds.shuffle()
-	speech_audio.stream = _fire_sounds.front()
-	if last_speech_line == speech_audio.stream:
-		return 
-	last_speech_line = speech_audio.stream   # Tracked to avoid repeating the same line
-	last_speech_type = SpeechType.FIRE
-	speech_audio.play()
+	_play_sound_from_library(AudioLibrary.CULTIST_VOICE_TYPE.FIRE)
 
 
 func play_snake_sound():
-	_snake_sounds.shuffle()
-	speech_audio.stream = _snake_sounds.front()
-	if last_speech_line == speech_audio.stream:
-		return 
-	last_speech_line = speech_audio.stream   # Tracked to avoid repeating the same line
-	last_speech_type = SpeechType.SNAKE
-	speech_audio.play()
+	_play_sound_from_library(AudioLibrary.CULTIST_VOICE_TYPE.SNAKE)
 
 
 func play_bomb_sound():
-	_bomb_sounds.shuffle()
-	speech_audio.stream = _bomb_sounds.front()
-	if last_speech_line == speech_audio.stream:
-		return 
-	last_speech_line = speech_audio.stream   # Tracked to avoid repeating the same line
-	last_speech_type = SpeechType.BOMB
-	speech_audio.play()
+	_play_sound_from_library(AudioLibrary.CULTIST_VOICE_TYPE.BOMB)
 
 
 func play_comet_sound():
-	_comet_sounds.shuffle()
-	speech_audio.stream = _comet_sounds.front()
-	if last_speech_line == speech_audio.stream:
-		return 
-	last_speech_line = speech_audio.stream   # Tracked to avoid repeating the same line
-	last_speech_type = SpeechType.COMET
-	speech_audio.play()
+	_play_sound_from_library(AudioLibrary.CULTIST_VOICE_TYPE.COMET)
 
 
 # Signalled from the idle loop Wait Random
@@ -456,3 +319,13 @@ func play_clamber_sound(clamber_in : bool) -> void:
 
 func on_player_detected(_player, _position) -> void:
 	play_detection_sound()
+
+## Generic function used to play a sound from the AudioLibrary
+func _play_sound_from_library(cultist_voice_type: AudioLibrary.CULTIST_VOICE_TYPE) -> void:
+	var sound = AudioLibrary.get_voicelines(AudioLibrary.CULTIST_VOICE_TYPE.FIRE).pick_random()
+	speech_audio.stream = sound
+	if last_speech_line == speech_audio.stream:
+		return 
+	last_speech_line = speech_audio.stream   # Tracked to avoid repeating the same line
+	last_speech_type = SpeechType.FIRE
+	speech_audio.play()

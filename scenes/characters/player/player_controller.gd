1d9e130547bd0c6a49e94a227b4ae7cb25f920f7
Mooses2k <moses2k@gmail.com>
2025-07-26 21:05:20 -0700

    Fix mouse sensitivity slider bar too close to left #668
    Resolved by adding a 0 in the right place. Waiting merge to new_character

--- scenes/characters/player/player_controller.gd
+++ scenes/characters/player/player_controller.gd
@@ -14,9 +14,9 @@ var moved_since_sprint : bool = false
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		var sens = InputSettings.setting_mouse_sensitivity*0.01
+		var sens = InputSettings.setting_mouse_sensitivity * 0.001
 		camera_pitch -= sens*event.relative.y
-		camera_pitch = clamp(camera_pitch, - PI*0.5, PI*0.5)
+		camera_pitch = clamp(camera_pitch, - PI * 0.5, PI * 0.5)
 		state.facing = state.facing.rotated(Vector3.UP, -sens*event.relative.x)
 
 func _process(_delta : float) -> void:
